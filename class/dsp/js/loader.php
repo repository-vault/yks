8fda7e921a85aac490ec7f87d9a0c2ce29c5331b
François Leurent <fleurent@ivsweb.com>
2009-01-05 17:39:46 +0000

    [Js] Encapsulation du Loader JS
    git-svn-id: http://svn.exyks.org/yks/trunk@131 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/dsp/js/loader.php
+++ class/dsp/js/loader.php
@@ -9,35 +9,74 @@ define("JS_CACHE_DIR",$js_cache_dir);
 $js_prefixs=array();
 $js_build_list=array();
 
-function build_js_cache($build_list,$prefixs,$compress=null){
-  $hash="";
 
+class Js {
+  static $prefixs;
+
+  static function realpath($file){
+    return strtr($file, self::$prefixs).'.js';
+  }
+
+
+  static function dynload($uid, $js_build_list, $commons_dir){
+
+    $build_list = array( $uid );
+    $list = glob("$commons_dir/mts/Headers/*.js");
+
+    foreach($list as $file){
+        $contents = file_get_contents($file);
+        unset($out);
+        $blk = "[\s\r\n]*";
+        $mask  = "#Doms.loaders\[\s*(['\"])(.*?)\\1\s*\]$blk=$blk(\{.*?\})#s";
+        if(preg_match_all($mask, $contents, $out, PREG_SET_ORDER))
+        foreach($out as $line) {
+            list(, , $key, $body) = $line;
+            if(!isset($js_build_list[$key]))continue;
+            $body = json_decode($body, true);
+            if(!$body) continue;
+            $body['active'] = $js_build_list[$key];
+            $js_build_list[$key] = $body;
+        }
+    }
+
+    $load = $js_build_list[$uid];
+    if(is_array($load['deps']))    //need true recursion stack here
+        foreach($load['deps'] as $dep)
+            array_unshift($build_list, $dep);
+
+
+    $build_list = array_map(array('Js','realpath'), $build_list);
+
+    return $build_list;
+  }
+
+
+  static function build($build_list, $compress){
+    $hash="";
         //generate hash based on mtime & filename
-  foreach($build_list as $k=>$file){
-	$build_list[$k] = $file = strtr($file,$prefixs);
-	if(!is_file($file) ) die("!! $file is unavaible");
-    $time=filemtime($file);
-	$hash.="$file:$time;";
-  } $hash=md5($hash);
+    foreach($build_list as $file){
+        if(!is_file($file) ) die("!! $file is unavaible");
+        $time = filemtime($file);
+        $hash.= "$file:$time;";
+    } $hash = md5($hash);
 
-  $cache_full=JS_CACHE_DIR."/{$hash}.uncompressed.js";
-  $cache_packed=JS_CACHE_DIR."/{$hash}.packed.js";
+    $cache_full   = JS_CACHE_DIR."/{$hash}.uncompressed.js";
+    $cache_packed = JS_CACHE_DIR."/{$hash}.packed.js";
+    $cache_file   = $compress ? $cache_packed : $cache_full;
+    if(is_file($cache_file)) return $cache_file;
 
-  $cache_file=$compress?$cache_packed:$cache_full;
-  if(is_file($cache_file)) return $cache_file;
+    //delete_dir(JS_CACHE_DIR,false);
+    create_dir(JS_CACHE_DIR);
 
-  delete_dir(JS_CACHE_DIR,false);
-  create_dir(JS_CACHE_DIR);
+    $contents="";
+    foreach($build_list as $file) $contents.=file_get_contents($file);
+    file_put_contents($cache_full, $contents);
 
-  $contents="";
-  foreach($build_list as $file) $contents.=file_get_contents($file);
-  file_put_contents($cache_full, $contents);
-
-  $cmd = JAVA_PATH." -jar ".YUI_COMPRESSOR_PATH.
-		" --charset UTF-8 -o $cache_packed  $cache_full 2>&1";
-  if($compress) exec($cmd, $out, $err);
-  if($err) die("$err : ".print_r($out,1));
-  return $cache_file;
+    $cmd = JAVA_PATH." -jar ".YUI_COMPRESSOR_PATH.
+        " --charset UTF-8 -o $cache_packed  $cache_full 2>&1";
+    if($compress) exec($cmd, $out, $err);
+    if($err) die("$err : ".print_r($out,1));
+    return $cache_file;
+  }
 }
-
-
+Js::$prefixs = &$js_prefixs;
