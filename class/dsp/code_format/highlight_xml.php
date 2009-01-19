64e21b6c2f635e63f292f53217023fc2503310a0
François Leurent <fleurent@ivsweb.com>
2009-01-19 21:41:11 +0000

    [HighLight/XML] Fonctionnalite no_pad sur script
    git-svn-id: http://svn.exyks.org/yks/trunk@138 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/dsp/code_format/highlight_xml.php
+++ class/dsp/code_format/highlight_xml.php
@@ -7,8 +7,9 @@ class highlight_xml {
   private $empty_node=false;
   private $inline_node=null;
 
-  private $no_pad = array("br", "img", "span", "b");
-
+  private $no_pad = array("script");
+  private $inline = array("br", "img", "span", "b", "a");
+  private $node_name;
 
   public  static function highlight($str){
     $highlighter = new self();
@@ -32,17 +33,17 @@ class highlight_xml {
 
   private function tag_open($parser, $name, $attribs) {
     $this->pos_pad('tag', $name);
-    $this->depth++;
+    $this->depth++; $this->node_name = $name;
 
     $this->feed("&lt;%s", $this->format($name, 'xml_tag') );
     if ($attribs) foreach ($attribs as $k => $v)
         $this->feed(' %s="%s"', $this->format($k,"xml_attr"), $this->format($v, 'xml_val'));
     $this->feed("&gt;"); 
-    $this->empty_node = true;
+    $this->empty_node = true; 
   }
 
   private function tag_close($parser, $name) {
-    $this->depth--;
+    $this->depth--; $this->node_name = false;
     if($this->empty_node) {
         $this->contents = preg_replace('/&gt;?$/', '',$this->contents);
         $this->feed("/&gt;");
@@ -55,7 +56,7 @@ class highlight_xml {
   private function cdata($parser, $str) {
     if(!trim($str)) return;
     $this->pos_pad('cdata');
-    $this->feed($str);
+    $this->feed(nl2br(trim(strtr($str,array(' '=>'&#160;')))));
   }
 
   private function std($parser, $str) {
@@ -73,9 +74,10 @@ class highlight_xml {
   private function format($str, $theme) { return "<span class='$theme'>$str</span>"; }
 
   private function pos_pad($type,  $name=false){
-    $inline_now = in_array($name, $this->no_pad) || in_array($type, array('cdata', 'std')) ;
+    $pad = in_array($this->node_name, $this->no_pad)?"":str_repeat("&#160;", $this->depth*2);
+    $inline_now = in_array($name, $this->inline) || in_array($type, array('cdata', 'std')) ;
     $nl = ! ($inline_now && $this->inline_node || is_null($this->inline_node) );
-    if($nl) $this->feed("<br/>".str_repeat("&#160;", $this->depth*2));
+    if($nl) $this->feed("<br/>$pad");
     $this->inline_node = $inline_now;
   }
 
