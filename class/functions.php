2e2849a5cfce662b2c9afec2910c4bb12b557437
François Leurent <fleurent@ivsweb.com>
2009-01-25 00:52:23 +0000

    [Yks] Cisaillement des fonctions de traitement des arrays dans stds/arrays.php
    git-svn-id: http://svn.exyks.org/yks/trunk@140 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/functions.php
+++ class/functions.php
@@ -10,6 +10,7 @@ require "$class_path/stds/jsx.php";
 require "$class_path/stds/date.php";
 require "$class_path/stds/data.php";
 require "$class_path/myks/input.php";
+require "$class_path/stds/arrays.php";
 
 if(defined('Yks')) rbx::$output_mode = yks::$href?0:1;
 
@@ -29,19 +30,6 @@ function bool($val,$str=false){
 }
 
 
-function make_tree($splat, $root=false, $parent_col ="parent", $children_col=false){
-    $tree=array(); $tmp=array();
-    while( list($id, $v)=each($splat) ){ $parent = $v[$parent_col]?$v[$parent_col]:$id;
-        if(!$tmp[$id]) $tmp[$id]=$children_col?$v:array();
-        elseif($children_col) $tmp[$id]=array_merge($v,$tmp[$id]); 
-        if($parent==$id ) {
-            if(!$tree[$parent]) $tree[$parent] = &$tmp[$id];
-            continue;
-        }
-        if($children_col) $tree[$parent][$children_col][$id] = &$tmp[$id];
-        else $tree[$parent][$id] = &$tmp[$id];
-    } return $root?array($root=>$tmp[$root]):$tree;
-}
 
 
 function ip2int($ip){return sprintf("%u",ip2long($ip));}
@@ -59,66 +47,22 @@ function fields($table){
 
 
 function between($a,$min,$max){return $a>=$min && $a<=$max; }
-function array_next_val($array,$val){return $array[array_search($val,$array)+1]; }
 
 function is_not_null($a){return !is_null($a);}
-function array_step($array,$val,$way=1,$loop=true){
-    $tmp=array_search($val,$array)+$way;
-    return $array[$loop?(($tmp+count($array))%count($array)):$tmp];
-}
 
 function preg_areplace($tmp, $str){ return preg_replace(array_keys($tmp),array_values($tmp),$str); }
 function preg_clean($filter, $str,$rem='^'){ return preg_replace("#[{$rem}{$filter}]#i",'',$str); }
 function preg_reduce($mask, $str){ preg_match($mask, $str, $out); return $out[1]; }
 
-function array_sort($array,$keys){
-    $keys = is_array($keys)?$keys:array_slice(func_get_args(),1);
-    return array_intersect_key(array_merge($keys=array_flip($keys),$array),$keys,$array);
-}
 
-    //!!dont sprintf($v,$k) ! bad thing © use mask_join else
-function array_mask($array,$mask){ foreach($array as &$v)$v=sprintf($mask,$v);return $array; }
-
-
-function mask_join($glue,$array,$mask){
-    foreach($array as $k=>&$v) $v=sprintf($mask,$v,$k);
-    return join($glue,$array);
-}
-
-function array_extract($array, $col){
-    $ret=array();
-    foreach($array as $v) $ret[]=$v[$col];
-    return array_unique($ret);
-}
-function array_get($array,$col){return $col?$array[$col]:$array; }
-
-function array_merge_numeric($a,$b){
-  foreach($b as $k=>$v)$a[$k]=is_array($v)&&is_array($a[$k])?array_merge($a[$k],$v):$v;return $a;
-}
-function attributes_to_assoc($x){$tmp=array(); //php 5.3 grrrr
-    foreach($x->attributes() as $k=>$v)$r[$k]=(string)$v;
-    return $r;
-}
 
 
 function specialchars_encode($v){ return htmlspecialchars($v,ENT_QUOTES,'utf-8'); }
 function specialchars_decode($str){ return htmlspecialchars_decode($str,ENT_QUOTES); }
 function specialchars_deep($v){return is_array($v)?array_map(__FUNCTION__,$v):specialchars_encode($v);}
 function mailto_escape($str){ return rawurlencode(utf8_decode(specialchars_decode($str))); }
-
-
-function array_sublinearize($a,$c){$ret=array();foreach($a as $k=>$val)$ret[$k]=$val[$c];return $ret;}
-
-
-
-
-
 function mail_valid($mail){ return (bool) filter_var($mail, FILTER_VALIDATE_EMAIL ); }
 
-
-function join_keys($array,$sep=' '){
-  $res=array();foreach(array_filter($array) as $k=>$v)$res[]="$k=\"$v\"";return join($sep,$res);
-}
 
 
 function reloc($url) {
@@ -139,36 +83,6 @@ function abort($code) {
         jsx::end();
     } reloc("?$dest");
 }
-
-
-function linearize_tree($tree,$depth=0){
-    $ret=array();
-    foreach($tree as $cat_id=>$children){
-        $ret[$cat_id]=array('id'=>$cat_id,'depth'=>$depth);
-        if($children)$ret+=linearize_tree($children,$depth+1);
-    }return $ret;
-}
-
-function array_sort_deep($array,$sort_by,$order='asort'){
-    $keys=array(); foreach($array as $k=>$v)$keys[$k]=$v[$sort_by]; $order($keys);
-    return array_merge_numeric($keys,$array);
-}
-function array_filter_deep($array,$sort_by,$val){
-    $keys=array(); foreach($array as $k=>$id)$keys[$k]=$id[$sort_by]; asort($keys);
-    return array_merge($keys,$array);
-}
-
-
-  // Find documentation in the manual
-function array_reindex($array,$cols=array()){
-    $res=array();if(!is_array($cols))$cols=array($cols);
-    foreach($array as $v){
-      $tmp=&$res;
-      foreach($cols as $col) $tmp=&$tmp[$v[$col]];
-      $tmp=$v;
-    }return $res;
-}
-
 
 function retrieve_constants($mask = "#.*?#", $format="{%s}"){
     $tmp = get_defined_constants (true); $tmp = $tmp['user']; $constants = array();
---
+++ class/stds/arrays.php
@@ -0,0 +1,84 @@
+<?
+function make_tree($splat, $root=false, $parent_col ="parent", $children_col=false){
+    $tree=array(); $tmp=array();
+    while( list($id, $v)=each($splat) ){ $parent = $v[$parent_col]?$v[$parent_col]:$id;
+        if(!$tmp[$id]) $tmp[$id]=$children_col?$v:array();
+        elseif($children_col) $tmp[$id]=array_merge($v,$tmp[$id]); 
+        if($parent==$id ) {
+            if(!$tree[$parent]) $tree[$parent] = &$tmp[$id];
+            continue;
+        }
+        if($children_col) $tree[$parent][$children_col][$id] = &$tmp[$id];
+        else $tree[$parent][$id] = &$tmp[$id];
+    } return $root?array($root=>$tmp[$root]):$tree;
+}
+
+function array_next_val($array,$val){ return array_step($array, $val, 1, false); }
+function array_step($array,$val,$way=1,$loop=true){
+    $tmp=array_search($val,$array)+$way;
+    return $array[$loop?(($tmp+count($array))%count($array)):$tmp];
+}
+
+function array_sort($array,$keys){
+    $keys = is_array($keys)?$keys:array_slice(func_get_args(),1);
+    return array_intersect_key(array_merge($keys=array_flip($keys),$array),$keys,$array);
+}
+
+    //!!dont sprintf($v,$k) ! bad thing © use mask_join else
+function array_mask($array,$mask){ foreach($array as &$v)$v=sprintf($mask,$v);return $array; }
+
+
+function mask_join($glue,$array,$mask){
+    foreach($array as $k=>&$v) $v=sprintf($mask,$v,$k);
+    return join($glue,$array);
+}
+
+
+function array_extract($array, $col){
+    $ret=array();
+    foreach($array as $v) $ret[]=$v[$col];
+    return array_unique($ret);
+}
+function array_get($array,$col){return $col?$array[$col]:$array; }
+
+function array_merge_numeric($a,$b){
+  foreach($b as $k=>$v)$a[$k]=is_array($v)&&is_array($a[$k])?array_merge($a[$k],$v):$v;return $a;
+}
+function attributes_to_assoc($x){$tmp=array(); //php 5.3 grrrr
+    foreach($x->attributes() as $k=>$v)$r[$k]=(string)$v;
+    return $r;
+}
+
+
+
+function array_sublinearize($a,$c){$ret=array();foreach($a as $k=>$val)$ret[$k]=$val[$c];return $ret;}
+
+
+
+function linearize_tree($tree,$depth=0){
+    $ret=array();
+    foreach($tree as $cat_id=>$children){
+        $ret[$cat_id]=array('id'=>$cat_id,'depth'=>$depth);
+        if($children)$ret+=linearize_tree($children,$depth+1);
+    }return $ret;
+}
+
+function array_sort_deep($array,$sort_by,$order='asort'){
+    $keys=array(); foreach($array as $k=>$v)$keys[$k]=$v[$sort_by]; $order($keys);
+    return array_merge_numeric($keys,$array);
+}
+function array_filter_deep($array,$sort_by,$val){
+    $keys=array(); foreach($array as $k=>$id)$keys[$k]=$id[$sort_by]; asort($keys);
+    return array_merge($keys,$array);
+}
+
+
+  // Find documentation in the manual
+function array_reindex($array,$cols=array()){
+    $res=array();if(!is_array($cols))$cols=array($cols);
+    foreach($array as $v){
+      $tmp=&$res;
+      foreach($cols as $col) $tmp=&$tmp[$v[$col]];
+      $tmp=$v;
+    }return $res;
+}
