425706754dd8e2fefde03993785e3a227af706e4
François Leurent <fleurent@ivsweb.com>
2009-06-19 21:18:29 +0000

    cleanups & css
    git-svn-id: http://svn.exyks.org/yks/trunk@237 b8ca103b-dd03-488c-9448-c80b36131af2

---
+++ class/myks/elements/drivers/pgsql/register_exts.php
@@ -0,0 +1,28 @@
+<?
+
+ //manual declaration of types, dont bother cleaning this
+
+    myks_gen::$type_resolver->register("in", array(
+      'boolean'=>'bool',
+      '"trigger"'=>'trigger', //vraiment ridicule postgres :/
+      'integer'=>'int',
+      'character varying'=>'string',
+      'timestamptz'=>'timestamptz',
+      'timestamp without time zone'=>'timestamp', //information_schema.routines.data_type
+      'bigint'=>'bigint',
+      'record'=>'record',
+    ));
+
+    myks_gen::$type_resolver->register("out", array(
+      'bool'=>'boolean',
+      'trigger'=>'trigger',
+      'string'=>'varchar',
+      'int'=>'integer',
+      'bigint'=>'bigint',
+      'timestamptz'=>'timestamptz',
+      'timestamp'=>'timestamp',
+      'record'=>'record',
+    ));
+
+
+return true;
