cbc04b3508c3ebe4b00e8ee039b9af45bf9dd496
François Leurent <fleurent@ivsweb.com>
2009-06-21 01:40:38 +0000

    simplexml_load_url
    git-svn-id: http://svn.exyks.org/exts/selectors@49 628ac6e6-79fc-11de-858e-113bbc11c6a4

--- functions.php
+++ functions.php
@@ -8,6 +8,11 @@ if(!function_exists('preg_reduce')) {
   }
 }
 
+function simplexml_load_url($url){
+    $str = file_get_contents($url);
+    return simplexml_load_html($str);
+}
+
 function simplexml_load_html($str, $class="Element"){
     libxml_use_internal_errors(true);
     $doc = new DomDocument("1.0", "UTF-8");
