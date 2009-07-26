c04c3292fbd066ac8f1d57f39734f33a3231b563
François Leurent <fleurent@ivsweb.com>
2009-07-26 16:25:55 +0000

    [silent]
    git-svn-id: http://svn.exyks.org/exts/browser/trunk@4 628ac6e6-79fc-11de-858e-113bbc11c6a4

---
+++ url.php
@@ -0,0 +1,64 @@
+<?
+
+class url  extends native {
+
+  function __construct($url){
+    $this->data  = urls::parse($url);
+    if(!$url) $this->empty = true;
+  }
+
+
+  function merge($url){
+    if($url->empty) return $this;
+    if($this->is_relative) return $url;
+
+    if($url->host && $this->host != $url->host) return $url;
+    if($url->scheme && $this->scheme  != $url->scheme) return $url;
+
+    $path = urls::paths_merge($this->path, $url->path);
+    $merge = array_filter(array(
+        'host'=>$this->host,
+        'scheme'=>$this->scheme,
+        'path'=>$path,
+        'query'=> $url->query?$url->query:($path!=$this->path?false:$this->query),
+        'fragment'=>$url->fragment,
+    ));
+    return new url($merge);
+  }
+
+  function is_ssl(){
+    return $this->scheme == 'https';
+  }
+
+  function is_relative(){
+    return substr($this->path,0,1)!="/";
+  }
+
+  function is_absolute(){
+    return !$this->is_relative;
+  }
+  function is_browsable(){
+    return isset($this['scheme']) && isset($this['host']) && isset($this['path']);
+  }
+
+  function get_hash($full = false){
+    if(!$this->scheme || !$this->host) return false;
+    $str = "{$this->scheme}://{$this->host}";
+    $str .=$this->path;
+    if($full){
+        if($this->query)$str.="?{$this->query}";
+        if($this->fragment)$str.="#{$this->fragment}";
+    } return $str;
+  }
+
+  function get_http_query(){
+    $str = $this->path;
+    if($this->query)$str.="?".($this->query);
+    return $str;
+  }
+
+
+  function __toString(){
+    return $this->get_hash(true);
+  }
+}
---
+++ xhr.php
@@ -0,0 +1,77 @@
+<?
+
+class xhr extends sock {
+  private $browser;
+  private $url;
+
+  function __construct($browser, $url){
+    $this->browser = $browser;
+
+    if(!$url->host)
+        throw new Exception("Invalid host");
+    $this->set_url($url);
+  }
+
+  function __get($key){
+    if($key=="url") return $this->url;
+  }
+
+  function execute($query){
+    $this->request($query->url->http_query, $query->method, $query->data, $query->headers);
+  }
+
+
+  private function set_url($url){
+    if($this->url->host && $url->host != $this->url->host)
+        $this->close();
+
+    $this->url     = $url;
+
+    $port    = $url->is_ssl?443:80;
+    $enctype = $url->is_ssl?'ssl://':'';
+
+    parent::__construct($url->host, $port, $enctype);
+  }
+
+
+  function process_response(){
+    $cookies_headers = (array) $this->response['headers']['Set-Cookie'];
+    foreach($cookies_headers as $header){
+        $cookie = cookie::from_header($header->value, $header->extras, $this->url);
+        $this->browser->store_cookie($this->url, $cookie);
+    }
+
+    if(in_array($this->response['code'], array(302,301) ) )
+        $this->follow_redirect();
+
+  }
+
+
+  function follow_redirect(){
+    $this->end_headers();
+    $location = new url((string) $this->response['headers']['Location']);
+    $url = $this->url->merge($location);
+    $this->set_url($url);
+    $this->request($this->url->http_query);
+  }
+
+  function forge_query_headers($headers = array()){
+
+    $headers = parent::forge_query_headers($headers);
+
+    $headers = array_merge($headers, $this->browser->ua->headers);
+
+    //$query_url = $this->query['url']; and $this->url are the same
+    $cookies = $this->browser->get_cookies($this->url);
+    if($cookies) {
+        $cookies_tmp = array();
+        foreach($cookies as $cookie)
+            $cookies_tmp []= "$cookie->name=$cookie->value";
+        $headers['Cookie'] = join('; ', $cookies_tmp);
+    }    
+
+    return $headers;
+  }
+
+}
+
