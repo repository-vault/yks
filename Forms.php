486c63cf32fbc413ee4f71496e85c2d7b3933750
François Leurent <fleurent@ivsweb.com>
2009-07-26 15:14:33 +0000

    function simplexml_load_html has to be a bit more complex to manage softly with non utf-8 pages Form.set is useless - and the whole dom modification is a bad concept when using simpleXML (read only)
    git-svn-id: http://svn.exyks.org/exts/selectors@53 628ac6e6-79fc-11de-858e-113bbc11c6a4

--- Forms.php
+++ Forms.php
@@ -3,12 +3,6 @@
 
 class Forms {
 
-  public static function set(Element $element, $data){
-    foreach($data  as $name=>$value)
-        $element->getElement("*[name='$name']")->addAttribute('value', $value);
-    return $element;
-  }
-
     //fast way to retrieve a form element by its submit value
   public static function getFormBySubmit(Element $element, $submit){
     $path = "//form[descendant::input[@type='submit'][@value='$submit']]";
@@ -21,12 +15,14 @@ class Forms {
 
   public static function toQueryString(Element $element){
     $queryString  = array();
-    foreach($element->getElements("input, select, textarea") as $el){
+    foreach($element->getElements("input[type!=file], select, textarea") as $el){
         if(!$el['name'] || $el['disabled']) continue;
-        $value = strtolower($el->tagName) == 'select' 
-            ? array_extract($el->getSelected(), "value")
-            : (($el['type'] == 'radio' || $el['type'] == 'checkbox') && !$el['checked'])
-                ? null : (string) $el['value'];
+        if(strtolower($el->getName()) == 'select') $value = array_extract($el->getSelected(), "value");
+        elseif(strtolower($el->getName()) == 'textarea') $value = $el->get("innerHTML");
+        elseif(in_array($el['type'], array('radio', 'checkbox')) && !$el['checked'])
+            $value = $el['value']?$el['value']:"on";
+        else $value = (string) $el['value'];
+
         $queryString [(string) $el['name']]  = $value;
     } return $queryString;
   }
