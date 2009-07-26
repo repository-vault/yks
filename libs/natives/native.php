535c6a1d6f093212a8ff5f5b40a415447e5a44d5
François Leurent <fleurent@ivsweb.com>
2009-07-26 15:04:05 +0000

    Libs evolution/factorisation : natives
    git-svn-id: http://svn.exyks.org/yks/trunk@262 b8ca103b-dd03-488c-9448-c80b36131af2

---
+++ libs/natives/native.php
@@ -0,0 +1,46 @@
+<?
+
+
+abstract class native implements ArrayAccess {
+  protected $data;
+
+  function __get($key){
+    if(isset($this->data[$key]))
+        return $this->data[$key];
+    if(  (substr($key,0,3)=="is_" && method_exists($this, $getter = $key))
+        || method_exists($this, $getter = "get_$key")
+        || $this->manager && method_exists($this->manager, $getter))
+        return $this->$getter();
+  }
+
+    //proxies to manager's static functions
+  function __call($method, $args){
+    if(!($this->manager && method_exists($this->manager, $method))) return;
+    array_unshift($args, $this);
+    return call_user_func_array(array($this->manager, $method), $args);
+  }
+
+
+  function offsetExists ($key){ return isset($this->data[$key])||isset($this->$key); }
+  function offsetGet($key){ return $this->$key;}
+  function offsetSet($offset,$value){}
+  function offsetUnset($key){ }
+ 
+}
+
+
+abstract class __event {
+
+  private $__events;
+  function fireEvent($event, $args){
+    $chain = (array) $this->__events[$event];
+
+    foreach($chain as $callback)
+        call_user_func_array($callback, $args);
+  }
+
+  function addEvent($event, $callback){
+    $this->__events[$event][] = $callback;
+  }
+
+}
