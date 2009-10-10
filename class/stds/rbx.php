1f242bffaec71c0713e84bd8a37a4c7309d3ae5e
François Leurent <fleurent@ivsweb.com>
2009-10-10 13:49:27 +0000

    rbx => cli
    git-svn-id: http://svn.exyks.org/yks/trunk@345 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/stds/rbx.php
+++ class/stds/rbx.php
@@ -3,10 +3,7 @@
 */
 
 
-
-
 class rbx extends Exception {
-  const pad = 70;
 
   static public $rbx=array();
   static public $output_mode=1; //1 = direct output,0 = JSX & YKS
@@ -23,33 +20,7 @@ class rbx extends Exception {
     if(self::$output_mode!=1) return;
     self::$rbx['log'].="$zone : $msg".LF;
 
-    echo self::pad($msg, ' ', STR_PAD_RIGHT, "%s: $zone").LF;
-  }
-
-  static function box($title, $msg){
-    $args = func_get_args(); $pad_len = self::pad;
-
-    for($a=1;$a<count($args);$a+=2) {
-      $msg= &$args[$a];
-      if(!is_string($msg)) $msg = print_r($msg, 1);
-      $msg = explode("\n", trim($msg));
-      $pad_len = max($pad_len, max(array_map('strlen', $msg))+2); //2 chars enclosure
-    }
-
-    for($a=0; $a<count($args); $a+=2) {
-      echo self::pad(" {$args[$a]} ", "═", STR_PAD_BOTH, $a?"╠%s╣":"╔%s╗", $pad_len).LF;
-      foreach($args[$a+1] as $line)
-          echo self::pad($line, " ", STR_PAD_RIGHT, "║%s║", $pad_len).LF;
-    }
-
-    echo self::pad('', "═", STR_PAD_BOTH, "╚%s╝", $pad_len).LF;
-  }
-
-  private static function pad($title='', $pad = '─', $MODE = STR_PAD_BOTH, $mask = '%s', $pad_len = self::pad){
-    $pad_len -= mb_strlen(sprintf($mask, $title));
-    $left = ($MODE==STR_PAD_BOTH) ? floor($pad_len/2) : 0;
-    return sprintf($mask, 
-            str_repeat($pad, max($left,0)) . $title . str_repeat($pad, max($pad_len - $left,0)));
+    echo cli::pad($msg, ' ', STR_PAD_RIGHT, "%s: $zone").LF;
   }
 
   static function delay(){ $_SESSION['rbx']=rbx::$rbx;rbx::$rbx=array(); }
@@ -62,26 +33,31 @@ class rbx extends Exception {
   }
   static function ok($msg,$jsx=0){ return new self("ok",$msg,$jsx); }
 
-  static function title($title){ echo self::pad(" $title ").LF; }
-  static function line(){ echo self::pad().LF.LF; }
+  static function title($title){ echo cli::pad(" $title ").LF; }
+  static function line(){ echo cli::pad().LF.LF; }
 
   static function init($max,$flag=false){
     self::$max=$max;
     if(self::$flag=$flag) data::store(self::$flag,0,600);
-    if(self::$output_mode) echo self::pad('', ' ', STR_PAD_RIGHT, '[%s]');
+    if(self::$output_mode) echo cli::pad('', ' ', STR_PAD_RIGHT, '[%s]');
     return self::$pos=0;
   }
+  static function box($title, $msg){
+    $args = func_get_args();
+    call_user_func_array(array('cli', 'box'), $args);
+  }
+
   static function walk($step){
     $current = round(  self::$max ? ($step/self::$max) : 1,3);
-    $step = floor($current* (self::pad-2) );
+    $step = floor($current * 100); //max 100 steps
     $old = self::$pos; self::$pos= $step;
     if($step == $old) return $current;
     if(self::$flag) return data::store(self::$flag, $current, 600);
 
     if(!self::$output_mode) return;
-    echo "\r".self::pad(str_repeat("─",$step), ' ', STR_PAD_RIGHT, "[%s]"); flush();
+    echo "\r".cli::pad(str_repeat("─", floor($current*(cli::pad-2))), ' ', STR_PAD_RIGHT, "[%s]"); flush();
     if($current == 1) echo LF;
   }
   function __toString(){ return $this->message; }
-  static function end(){ if(self::$pos!=self::pad) self::walk(self::$max); }
+  static function end(){ if(self::$pos!=100) self::walk(100); }
 }
