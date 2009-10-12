0ee888a97984a81b6b10de747ea723c9cd59e0d9
François Leurent <fleurent@ivsweb.com>
2009-10-12 12:27:31 +0000

    Removing Tables =>3rd party
    git-svn-id: http://svn.exyks.org/yks/trunk@350 b8ca103b-dd03-488c-9448-c80b36131af2

--- base/subs/Admin/Tables/home.php
+++
@@ -1,8 +0,0 @@
-<?
-
-sql::select("ks_queries_list");
-$queries_list = sql::brute_fetch('query_id');
-
-sql::select("ks_queries_params",true,"query_id, query_id AS has_parameters", "GROUP BY query_id");
-$queries_list = array_merge_numeric($queries_list,  sql::brute_fetch("query_id"));
-
--- base/tpls/Admin/Tables/params.tpl
+++
@@ -1,6 +0,0 @@
-<box theme="as" class="center" caption="Paramétrage de la requète">
-
-<ks_form ks_action="query_params_set" submit="Appliquer">
- <fields><?=join(CRLF, $params_str)?></fields>
-</ks_form>
-</box>
