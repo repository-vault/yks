f4e21103b0165115767d0f32ac7eff418ca0e224
François Leurent <fleurent@ivsweb.com>
2009-10-20 23:00:59 +0000

    Myks => cli
    git-svn-id: http://svn.exyks.org/yks/trunk@358 b8ca103b-dd03-488c-9448-c80b36131af2

--- base/subs/Yks/Scripts/Myks/main.php
+++ base/subs/Yks/Scripts/Myks/main.php
@@ -4,39 +4,18 @@ while(@ob_end_clean());
 
 exyks::$page_def = "_all"; //tpls is not loaded
 
-define('XML_CACHE_PATH', CACHE_PATH."/xml");
-define('XSL_CACHE_PATH', CACHE_PATH."/xsl");
-
-include_once "$class_path/sql/".SQL_DRIVER.".php";
-include_once "$class_path/stds/files.php";
-include_once "$class_path/dom/dtds.php";
-include_once "$class_path/myks/parser.php";
-include_once "$class_path/myks/elements.php";
-include_once "$class_path/myks/generator.php";
-include_once "$class_path/xsl/generator.php";
-
-$myks_config = config::retrieve("myks");
-$privileges = $myks_config->privileges;
-
-privileges::declare_root_privileges($privileges);
+$limited = $argv0;
 
 
+$myks_runner = new myks_runner();
 
-include "cache_dir.php";
+$cli_commands = array('manage_types', 'manage_locales'); //for CLI tunnel..
 
-try {
-    $process_sql = true;
-    sql::connect($myks_config['link_admin']);
-}catch(rbx $e){
-    $process_sql = false;
-    rbx::ok("Skipping SQL management");
+if(in_array($limited, $cli_commands)) {
+    call_user_func(array($myks_runner, $limited));
+    die;
 }
 
+$myks_runner->manage_types();
 
-$xml_filename    = RSRCS_PATH."/xsl/root.xsl";        //meta XSL source
-$xsl_filename    = RSRCS_PATH."/xsl/metas/xsl_gen.xsl";     //meta XSL stylesheet
-$mykse_file_path = XML_CACHE_PATH."/myks.xml";
-$mykse_file_url  = "$site_url/".ltrim(end(explode(WWW_PATH, $mykse_file_path,2)),"/");    //W00t
-
-$browsers_engine = array( 'trident', 'gecko', 'webkit', 'presto');
-$rendering_sides = array( 'server', 'client');
+//die(sys_end(exyks::tick('generation_start')));
