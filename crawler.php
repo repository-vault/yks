a3b4cb30264715769f4d2f4ddcb220162c467e6d
François Leurent <fleurent@ivsweb.com>
2009-10-31 01:59:26 +0000

    crawler
    git-svn-id: http://svn.exyks.org/exts/browser/trunk@32 628ac6e6-79fc-11de-858e-113bbc11c6a4

---
+++ crawler.php
@@ -0,0 +1,44 @@
+<?
+
+class crawler extends window {
+  protected $urls = array();
+
+    //extend go to stack urls
+  function go($url, $method = 'GET', $data = array() ){
+    $this->urls[$url] = true;
+
+    parent::go($url, $method, $data);
+
+    $str = $this->document->asXML();
+    return $this->parse_contents($str, $url)?$url:false;
+  }
+  
+  function parse_contents($str){
+    return false;
+  }
+
+  function walk(){
+    return reset(array_keys($this->urls, false));
+  }
+
+  function skip($url){
+    $this->urls[$url] = true;
+  }
+
+  function scan_for_pages(){
+  
+  
+    $links_dom = $this->document->getElements("a[href^='/']");//, a[href^='v']
+    foreach($links_dom as $a){
+        $url = $this->parse_url((string) $a["href"]);
+        $this->enqueue_url($url);
+    }
+    print_r($this->urls);
+  }
+  function enqueue_url($url){
+    if($url && !$this->urls[$url])
+            $this->urls[$url] = false;
+  }
+
+  function parse_url($url) { return $url; }
+}
