be240cd7d1c365cc5197a74245ac644226204820
François Leurent <fleurent@ivsweb.com>
2010-02-04 23:45:55 +0000

    cl runner : updates
    git-svn-id: http://svn.exyks.org/yks/trunk@418 b8ca103b-dd03-488c-9448-c80b36131af2

--- cl.php
+++ cl.php
@@ -1,6 +1,8 @@
 <?
+
 $host  = $argv[1];
 $entry = $argv[2];
+$args  = array_slice($argv,3);
 
 $_SERVER['SERVER_NAME']     = $host ? $host : 'cli';
 
@@ -15,11 +17,11 @@ $z_e_r_o_entries = array(
   'yks'  => 'yks_runner',
   'myks' => 'myks_runner',
   'sync' => 'sync_runner',
+  'sql'  => 'sql_runner',
 ); $entry = $z_e_r_o_entries[pick_in($entry, array_keys($z_e_r_o_entries))];
 
 
 
 
 
-
-interactive_runner::start($entry);
+interactive_runner::start($entry, $args);
