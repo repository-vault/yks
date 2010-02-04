ff1889ef32d87bde512a1c100aec2e22a47db228
François Leurent <fleurent@ivsweb.com>
2010-02-04 23:34:49 +0000

    Config -> search avoid creation
    git-svn-id: http://svn.exyks.org/yks/trunk@414 b8ca103b-dd03-488c-9448-c80b36131af2

--- base/subs/Yks/Wysiwyg/main.php
+++ base/subs/Yks/Wysiwyg/main.php
@@ -10,11 +10,11 @@ include_once "$class_path/users/users.php";
 include_once "$class_path/auth/auth.php";
 
 
-$upload_flag=preg_clean("a-z0-9",$sub0);
-$upload_src=$sub1;
+$upload_flag = preg_clean("a-z0-9",$sub0);
+$upload_src  = $sub1;
 
-$upload_type=preg_clean("a-z0-9_",$sub2);
-$upload_def=$config->upload->$upload_type;
+$upload_type = preg_clean("a-z0-9_",$sub2);
+$upload_def  = $config->upload->search($upload_type);
 
 if(!FILE && JSX && $upload_flag) die(json_encode(apc_fetch("upload_$upload_flag")));
 elseif(!$upload_flag) $upload_flag=crpt(sess::$sess['user_id']._NOW,FLAG_UPLOAD,10);
