53d327a38e18fe3e570f1ced0a5f7a1b10542b11
François Leurent <fleurent@ivsweb.com>
2010-02-08 23:34:43 +0000

    git-svn-id: http://svn.exyks.org/yks/trunk@463 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/exyks/module.php
+++ class/exyks/module.php
@@ -7,12 +7,13 @@ class exyks_module {
   private $key;
   private $ns;
   private $module_root;
+  private $module_rq_start;  //request start for the module
+  private $module_sg_start;  //module suggested start
 
   function __construct($module_xml){
 
     $this->key = $module_xml['key'];
     $this->ns  = "module/$this->key"; //or ...
-
 
     $tmp = $module_xml['manifest'];
 
@@ -28,6 +29,9 @@ class exyks_module {
 
     $this->manifest_xml  = ksimplexml::load_file($this->manifest_file);
 
+    $this->module_sg_start = $this->manifest_xml['start'];
+    $this->module_rq_start = pick($module_xml['start'], $this->module_sg_start);
+
     $this->module_root   = dirname($this->manifest_file); //or ...
 
         //two way
@@ -42,6 +46,9 @@ class exyks_module {
 
   private function get_virtual_paths(){
     $paths = array();
+    if($this->module_sg_start)
+        $paths[$this->module_rq_start] = array($this->module_root, $this->module_sg_start);
+
     foreach($this->manifest_xml->paths->iterate("path") as $path) {
         $path_key = $path['symbolic'];
 
