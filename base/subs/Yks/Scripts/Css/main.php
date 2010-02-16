d4c68674f94ffa9475941585364bd5d05423f5f7
François Leurent <fleurent@ivsweb.com>
2010-02-16 14:47:38 +0000

    Delete css old
    git-svn-id: http://svn.exyks.org/yks/trunk@518 b8ca103b-dd03-488c-9448-c80b36131af2

--- base/subs/Yks/Scripts/Css/main.php
+++
@@ -1,9 +0,0 @@
-<?
-include "$class_path/dsp/css/compressor.php";
-include_once "$class_path/stds/files.php";
-
-
-$file_url = "http://commons.newdev.local/css/Ivs/global.css";
-//$file_url="/css/Ivs/global.css";
-$str=compute_css($file_url);
-die($str);
