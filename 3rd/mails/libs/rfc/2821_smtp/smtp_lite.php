d72b66b39ea7bceeb2bbcddddbd6c2ad59e2192c
François Leurent <fleurent@ivsweb.com>
2010-03-31 15:30:18 +0000

    Encapsulate mini_send in smtp_lite
    git-svn-id: http://svn.exyks.org/yks/trunk@588 b8ca103b-dd03-488c-9448-c80b36131af2

--- 3rd/mails/libs/rfc/2821_smtp/smtp_lite.php
+++ 3rd/mails/libs/rfc/2821_smtp/smtp_lite.php
@@ -1,13 +1,12 @@
 <?php
 
-
-
-function server_sync($sock,$response){
+class smtp_lite {
+  private static function server_sync($sock,$response){
     while(!preg_match("#^[0-9]{3}(?=\s)#",$tmp=fgets($sock,256),$out) );
     if($out[0]!=$response) throw rbx::error("Sync {$out[0]}!={$response} : $tmp");
-}
+  }
 
-function smtpmail($to, $subject, $body, $headers = TYPE_TEXT){
+  public static function smtpmail($to, $subject, $body, $headers = TYPE_TEXT){
     $contents = $headers.CRLF."Subject: $subject".CRLF;
     $contents.= "From: [".SITE_DOMAIN."] <webmaster@".SITE_DOMAIN.">".CRLF;
     $contents.= "To: $to".CRLF;
@@ -15,10 +14,10 @@ function smtpmail($to, $subject, $body, $headers = TYPE_TEXT){
 
     $name_mask = "#<\s*([^<]*)\s*>#";
     $to = preg_match($name_mask, $to,$out)?$out[1]:$to;
-    return smtpsend($contents, array($to));
-}
+    return self::smtpsend($contents, array($to));
+  }
 
-function smtpsend($contents,$dests){
+  public function smtpsend($contents, $dests){
     $smtp_config = yks::$get->config->apis->smtp;
     if(!$smtp_config)
         return false;
@@ -26,29 +25,29 @@ function smtpsend($contents,$dests){
     $smtp_sender = $smtp_config['sender'];
 
     $sock=fsockopen($smtp_config['host'], 25, $errno, $errstr, 20);
-    server_sync($sock, "220");
+    self::server_sync($sock, "220");
     fputs($sock, "EHLO ".$smtp_config['host'].CRLF);
-    server_sync($sock, "250");
+    self::server_sync($sock, "250");
 
     fputs($sock, "AUTH LOGIN".CRLF);
-    server_sync($sock, "334");
+    self::server_sync($sock, "334");
 
     fputs($sock, base64_encode($smtp_config['login']).CRLF);
-    server_sync($sock, "334");
+    self::server_sync($sock, "334");
 
     fputs($sock, base64_encode($smtp_config['pass']).CRLF);
-    server_sync($sock, "235");
+    self::server_sync($sock, "235");
 
     fputs($sock, "MAIL FROM: <$smtp_sender>".CRLF);
-    server_sync($sock, "250");
+    self::server_sync($sock, "250");
 
     foreach($dests as $mail_to){
         fputs($sock, "RCPT TO: <$mail_to>".CRLF);
-        server_sync($sock, "250");
+        self::server_sync($sock, "250");
     }
         
     fputs($sock, "DATA".CRLF);
-    server_sync($sock, "354");
+    self::server_sync($sock, "354");
 
    $message = $contents;
    $message.= CRLF.".".CRLF;
@@ -56,10 +55,15 @@ function smtpsend($contents,$dests){
         //die($message);
     fputs($sock, $message);
 
-    server_sync($sock, "250");
+    self::server_sync($sock, "250");
     fputs($sock, "QUIT\r\n");
     fclose($sock);
     return true;
+  }
+
+
+
 }
+
 
 
