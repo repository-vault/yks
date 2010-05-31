28d81d941080d5b49891def53a084c9b9456d068
François Leurent <fleurent@ivsweb.com>
2010-05-31 17:22:55 +0000

    Null value mananement
    git-svn-id: http://svn.exyks.org/yks/trunk@727 b8ca103b-dd03-488c-9448-c80b36131af2

--- rsrcs/js/3rd/usage/DatePicker.js
+++ rsrcs/js/3rd/usage/DatePicker.js
@@ -6,12 +6,19 @@ var DatePicker = new Class({
         pickerClass: 'datepicker_vista',
         format:'d/m/Y',
         allowEmpty:true
+
     };
 
     if(element.hasClass('input_time')) {
         tmp.format = 'd/m/Y H:i';
         tmp.timePicker = true;
     }
+
+    tmp.inputOutputFormat = tmp.format ;
+
+    if(! element.get('value').match(/^[ 0-9/:]*$/))
+        element.set('value', '');
+
     this.parent(element, tmp);
 
   }
