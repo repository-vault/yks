2ba4fbf8612331a3714aab8b165792bf53ebce24
François Leurent <fleurent@ivsweb.com>
2010-06-25 02:20:56 +0000

    Fix
    git-svn-id: http://svn.exyks.org/yks/trunk@767 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/sql/kpgsql.php
+++ class/sql/kpgsql.php
@@ -3,82 +3,82 @@
 class ksql extends isql {
 
   static function init(){
-    self::$pfx['search']  = "#`(.*?)`#";
-    self::$pfx['replace'] = "\"$1\"";
+    ksql::$pfx['search']  = "#`(.*?)`#";
+    ksql::$pfx['replace'] = "\"$1\"";
   }
 
   static function connect(){
-    $serv = self::$links_xml->search(self::$link);
+    $serv = ksql::$config->links->search(ksql::$link);
 
     if(!$serv['port'])$serv['port']= 5432;
     $sql_infos = "host='{$serv['host']}' port={$serv['port']}"
                 ." dbname='{$serv['db']}' user='{$serv['user']}' password='{$serv['pass']}'";
 
-    self::$links[self::$link] = pg_connect($sql_infos);
-    if(!self::$links[self::$link])
-      throw new Exception("Unable to load link #{".self::$link."} configuration");
+    ksql::$links[ksql::$link] = pg_connect($sql_infos);
+    if(!ksql::$links[ksql::$link])
+      throw new Exception("Unable to load link #{".ksql::$link."} configuration");
 
-    return self::$links[self::$link];
+    return ksql::$links[ksql::$link];
   }
 
   static function close($link = false){
-    if(!$link) $link = self::$link;
-    if(!($serv = self::$links[$link])) return;
-    pg_close($serv); unset(self::$links[$link]);
+    if(!$link) $link = ksql::$link;
+    if(!($serv = ksql::$links[$link])) return;
+    pg_close($serv); unset(ksql::$links[$link]);
   }
 
   static function free(&$r=null){
-    if($r=$r?$r:self::$result) pg_free_result($r);
+    if($r=$r?$r:ksql::$result) pg_free_result($r);
     return $r=null;
   }
 
 
   static function query($query, $arows=false){
-    if(!$lnk = self::get_lnk()) return false;
+    if(!$lnk = ksql::get_lnk()) return false;
 
-    $query = self::unfix($query);
-    self::$result = pg_query($lnk, $query);
+    $query = ksql::unfix($query);
+    ksql::$result = pg_query($lnk, $query);
 
-    if(self::$log) self::$queries[] = $query;
-    if(self::$result===false) {
-        $error = self::error(htmlspecialchars($query));
+    if(ksql::$log) ksql::$queries[] = $query;
+    if(ksql::$result===false) {
+        $error = ksql::error(htmlspecialchars($query));
         return $error;
     }
 
     if($arows) {
-        $arows = pg_affected_rows(self::$result);
+        $arows = pg_affected_rows(ksql::$result);
         return $arows; 
     }
-    return self::$result;
+    return ksql::$result;
   }
 
   static function fetch($r=false){
-    $tmp = pg_fetch_assoc( pick($r, self::$result));
+    $tmp = pg_fetch_assoc( pick($r, ksql::$result));
     return $tmp?$tmp:array();
   }
   
 
   static function fetch_all(){
-    return pg_fetch_all_columns(self::$result);
+    return pg_fetch_all_columns(ksql::$result);
   }
 
 
   static function error($msg=''){
-    $pg_error = pg_last_error(self::$links[self::$link]);
+    $pg_error = pg_last_error(ksql::$links[ksql::$link]);
     $msg = "<b>".htmlspecialchars($pg_error)."</b> in $msg";
-    if(DEBUG && !self::$transaction) error_log($msg);
+    if(DEBUG && !ksql::$transaction) error_log($msg);
     return false;
   }
 
 
-  static function rows($r=false){ return  pg_num_rows(pick($r, self::$result)); }
+  static function rows($r=false){ return  pg_num_rows(pick($r, ksql::$result)); }
   static function auto_indx($table){
-    $name = self::resolve($table);
+    $name = ksql::resolve($table);
     return (int)ksql::qvalue("SELECT auto_increment_retrieve('{$name['name']}')");
   }
 
   static function query_raw($query){
-    if(!$lnk = self::get_lnk()) return false;
+    if(!$lnk = ksql::get_lnk()) return false;
     return pg_query($lnk, $query);
   }
 
