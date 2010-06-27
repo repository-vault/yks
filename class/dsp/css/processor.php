e0436db3c9abc3fe6ac3f4f60676438a948c18bb
François Leurent <fleurent@ivsweb.com>
2010-06-27 21:35:09 +0000

    css processor boxes registration
    git-svn-id: http://svn.exyks.org/yks/trunk@772 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/dsp/css/processor.php
+++ class/dsp/css/processor.php
@@ -7,40 +7,61 @@ class css_processor {
   private $file_directory;
   private $file_contents;
 
+  static function init(){
+    classes::register_class_path("css_parser", "exts/css/parser.php");
+    classes::register_class_path("css_box", "exts/css/box.php");
+  }
+
   function __construct($uri, $contents = false){
     $this->file_uri       = $uri;
 
-    $this->file_path      = exyks_paths::resolve($this->file_uri);
+    //$this->file_path      = exyks_paths::resolve($this->file_uri);
 
     $this->file_name      = basename($file);
     $this->file_directory = dirname($file);
 
-    if(!$contents) $contents = file_get_contents($this->file_path);
-    $this->file_contents  = $contents;
+    //if(!$contents) $contents = file_get_contents($this->file_path);
+
+    $this->css            = css_parser::parse_file($this->file_uri);
   }
 
   function output(){
+    $this->resolve_boxes();
+    $this->resolve_imports();
     $this->resolve_externals();
-    $this->resolve_refs();
-    return $this->file_contents;
+    echo $this->css->output();
   }
-  
+
+  private function resolve_boxes(){
+    $boxes = $this->css->xpath("//rule[starts-with(@name,'box')]/parent::*");
+    foreach($boxes as $box) {
+        $box = new css_box($this->css, $box);
+        $box->write_cache();
+    }
+  }
+
+  private function resolve_imports() {
+    $imports = $this->css->xpath("//atblock[@keyword='import']");
+    foreach($imports as $import) {
+        $url = trim($import->expressions, "'\"");
+        $path = exyks_paths::merge(dirname($this->file_uri).'/', $url);
+        $path = exyks_paths::expose($path);
+        $import->set_expression("\"$path\"");
+    }
+  }
+
   private function resolve_externals(){
-    $mask  = "#url\(\s*(?:\"([^\"]*)\"|'([^']*)\'|([^)]*))\s*\)#";
-    $match = preg_match_all($mask, $this->file_contents, $out, PREG_SET_ORDER);
-    if(!$match) return;
-    $matches = array();
-    foreach($out as $m) $matches[$m[0]] = pick($m[1], $m[2], $m[3]);
-
-    //path://users/www/Users/users.css
-
-    foreach($matches as $replace=>&$val) {
-        $val = exyks_paths::merge(dirname($this->file_uri).'/', $val);
+    $externals = $this->css->xpath("//rule[starts-with(.,'url')]"); //!
+    foreach($externals as $external) {
+        $mask  = "#url\(\s*(?:\"([^\"]*)\"|'([^']*)\'|([^)]*))\s*\)#";
+        if(!preg_match($mask, (string)$external, $out))
+            continue;
+        $url  = pick($out[1], $out[2], $out[3]);
+        $val = exyks_paths::merge(dirname($this->file_uri).'/', $url);
         $val = exyks_paths::expose($val);
         $val = "url(\"$val\")";
+        $external->set_value($val);
      }
-
-    $this->file_contents = strtr($this->file_contents, $matches);
   }
 
 //inline style rewrite callback
@@ -51,20 +72,4 @@ class css_processor {
     $node->nodeValue= $contents;
   }
 
-  private function resolve_refs() {
-    $mask  = "#^@import\s+(?:\"([^\"]*)\"|'([^']*)$\')\s*(;|\$)#m";
-    $match = preg_match_all($mask, $this->file_contents, $out, PREG_SET_ORDER);
-    if(!$match) return;
-
-    $matches = array();
-    foreach($out as $m) $matches[$m[0]] = pick($m[1], $m[2]);
-
-    foreach($matches as $replace=>&$val) {
-        $val = exyks_paths::merge(dirname($this->file_uri).'/', $val);
-        $val = exyks_paths::expose($val);
-        $val = "@import \"$val\";";
-     }
-
-    $this->file_contents = strtr($this->file_contents, $matches);
-  }
 }
