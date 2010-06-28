40229ab43fd3213c00aab4e095dcda5b75d0ab34
François Leurent <fleurent@ivsweb.com>
2010-06-28 09:12:57 +0000

    css compressor bugfix
    git-svn-id: http://svn.exyks.org/yks/trunk@773 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/dsp/css/processor.php
+++ class/dsp/css/processor.php
@@ -1,18 +1,18 @@
 <?
 
-
 class css_processor {
   private $file_path;
   private $file_name;
   private $file_directory;
   private $file_contents;
+  private $css;
 
   static function init(){
     classes::register_class_path("css_parser", "exts/css/parser.php");
     classes::register_class_path("css_box", "exts/css/box.php");
   }
 
-  function __construct($uri, $contents = false){
+  private function __construct($uri, $contents = false){
     $this->file_uri       = $uri;
 
     //$this->file_path      = exyks_paths::resolve($this->file_uri);
@@ -25,7 +25,14 @@ class css_processor {
     $this->css            = css_parser::parse_file($this->file_uri);
   }
 
-  function output(){
+
+  static function delivers($path){
+    $process = new css_processor($path);
+    echo $process->output();
+  }
+
+
+  private function output(){
     $this->resolve_boxes();
     $this->resolve_imports();
     $this->resolve_externals();
@@ -51,16 +58,19 @@ class css_processor {
   }
 
   private function resolve_externals(){
-    $externals = $this->css->xpath("//rule[starts-with(.,'url')]"); //!
+    $externals = $this->css->xpath("//val[starts-with(.,'url')]/parent::*"); //!
     foreach($externals as $external) {
-        $mask  = "#url\(\s*(?:\"([^\"]*)\"|'([^']*)\'|([^)]*))\s*\)#";
-        if(!preg_match($mask, (string)$external, $out))
-            continue;
-        $url  = pick($out[1], $out[2], $out[3]);
-        $val = exyks_paths::merge(dirname($this->file_uri).'/', $url);
-        $val = exyks_paths::expose($val);
-        $val = "url(\"$val\")";
-        $external->set_value($val);
+
+        foreach($external->values as $i=>$value) {
+            $mask  = "#url\(\s*(?:\"([^\"]*)\"|'([^']*)\'|([^)]*))\s*\)#";
+            if(!preg_match($mask, (string)$value, $out))
+                continue;
+            $url  = pick($out[1], $out[2], $out[3]); $start = $out[0];
+            $val = exyks_paths::merge(dirname($this->file_uri).'/', $url);
+            $val = exyks_paths::expose($val);
+            $val = "url(\"$val\")";
+            $external->set_value($val, $i);
+        }
      }
   }
 
