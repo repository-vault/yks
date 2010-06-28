a00618ccaf6aeb5cf6da9b174de1bdfffba58e76
François Leurent <fleurent@ivsweb.com>
2010-06-28 16:42:20 +0000

    New mini lib helper : Soap Wrapper (unserialize/serialize)
    git-svn-id: http://svn.exyks.org/yks/trunk@780 b8ca103b-dd03-488c-9448-c80b36131af2

---
+++ libs/serializedsoapclient.php
@@ -0,0 +1,9 @@
+<?
+
+class SerializedSoapClient extends SoapClient {
+  function __call($func, $args){
+    $result = call_user_func_array( array('parent', '__call') , array($func, $args));
+    return unserialize($result);
+  }
+}
+
