cf7249fee86092305c3f80e2e452f2b7ef0716a4
François Leurent <fleurent@ivsweb.com>
2010-07-09 07:47:52 +0000

    Update geodecode base interface
    git-svn-id: http://svn.exyks.org/yks/trunk@807 b8ca103b-dd03-488c-9448-c80b36131af2

--- 3rd/geomaps/libs/geotools.php
+++ 3rd/geomaps/libs/geotools.php
@@ -11,59 +11,85 @@ class geotools {
   }
 
 
-  static function geodecode_addr($addr_infos, $addr_hash = null){
+  public static function geodecode_addr($addr_infos, $addr_hash = false) {
 
-    try {
-      $data = array(
-          'street'  => $addr_infos['addr_field1'],
-          'city'    => $addr_infos['addr_city'],
-          'country'  => $addr_infos['country_code'],
-          //'state'=>city=Sunnyvale&state=CA";        
-      ); $data_str = http_build_query($data);
+    $bypass_cache = $addr_hash === true; //debug
 
-      if(!$addr_hash)
-        $addr_hash = substr(md5($data_str),0,5);
+    $addr = array(
+      'street'  => $addr_infos['addr_field1'],
+      'city'    => $addr_infos['addr_city'],
+      'zip'     => $addr_infos['addr_zipcode'],
+      'country' => $addr_infos['country_code'],
+      'state'   => $addr_infos['addr_state'],
+    ); $data_str = http_build_query($addr);
 
-      $verif_hash = array('geodetic_hash' => $addr_hash);
-      $cached = sql::row(self::cache_table, $verif_hash);
-
-      if($cached) {
-        $data = array(
-          'lat' => $cached['geodetic_lat'],
-          'lon' => $cached['geodetic_lon']
-        );
-        return $data;
-      }
-
-      $data['appid'] = 'YD-9G7bey8_JXxQP6rxl.fBFGgCdNjoDMACQA--';
-      $data = http_build_query($data);
-
-
-      $url_base =  "http://local.yahooapis.com/MapsService/V1/geocode";
-      $query_url = "$url_base?$data";
-
-      $response_str = file_get_contents($query_url);
-      //echo $query_url.CRLF.$response_str;die;
-
-      $xml = simplexml_load_string($response_str);
-
-      if(!$xml->Result)
-        throw new Exception("Invalid response");
-
-      $lat = (float)$xml->Result->Latitude;
-      $lon = (float)$xml->Result->Longitude;
-
-      $data = array(
-        'geodetic_hash' => $addr_hash,
-        'geodetic_addr' => $data_str,
-        'geodetic_lat'  => $lat,
-        'geodetic_lon'  => $lon,
-      ); sql::insert("ks_geodecode_cache", $data);
-
-    } catch(Exception $e){
-
+    if($bypass_cache) {
+        error_log("Georequest bypassing cache !");
+        return self::geodecode_request($addr);
     }
 
+    if(!$addr_hash)
+        $addr_hash = substr(md5($data_str),0,5);
+
+    $verif_hash = array('geodetic_hash' => $addr_hash);
+    $cached = sql::row(self::cache_table, $verif_hash);
+
+    if($cached)
+        return array(
+            'lat' => $cached['geodetic_lat'],
+            'lon' => $cached['geodetic_lon']
+        );
+
+    try {
+        $geodetic = self::geodecode_request($addr);
+    } catch(Exception $e){
+        throw new Exception("Failure");
+    }
+
+        //update cache
+    $data = array(
+        'geodetic_hash' => $addr_hash,
+        'geodetic_addr' => $data_str,
+        'geodetic_lat'  => $geodetic['lat'],
+        'geodetic_lon'  => $geodetic['lon'],
+    );// sql::insert("ks_geodecode_cache", $data);
+
+    return $geodetic;
+  }
+
+  private static function geodecode_request($addr){
+
+    $data = array(
+      'street'  => $addr['street'],
+      'city'    => $addr['city'],
+      'postal'  => $addr['zip'],
+      'country' => $addr['country'],
+      'state'   => $addr['state'],
+    ); array_filter($data);
+
+    $data['appid'] = 'YD-9G7bey8_JXxQP6rxl.fBFGgCdNjoDMACQA--';
+    $data['appid'] = yks::$get->config->apis->geodecode['key'];
+
+
+    $data['flags'] = 'C';
+
+    $data = http_build_query($data);
+
+    $url_base =  "http://where.yahooapis.com/geocode";
+    $query_url = "$url_base?$data";
+
+    $response_str = strtolower(file_get_contents($query_url));
+    //echo $query_url.CRLF.$response_str;die;
+
+    $xml = simplexml_load_string($response_str);
+
+    if(!$xml->result)
+    throw new Exception("Invalid response");
+
+    $quality = (int)$xml->result->quality;
+
+    $lat = (float)$xml->result->latitude;
+    $lon = (float)$xml->result->longitude;
     return compact('lat', 'lon');
   }
 
