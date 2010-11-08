9223ed4e47eebbd03d71ded479d044edca6b9fcf
François Leurent <fleurent@ivsweb.com>
2010-11-08 16:48:57 +0000

    AuthDrupal endpoints rotations
    git-svn-id: http://svn.exyks.org/yks/trunk@948 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/apis/drupal/AuthDrupal.php
+++ class/apis/drupal/AuthDrupal.php
@@ -1,6 +1,9 @@
 <?
 /** Client side 
-  <drupal auth_wsdl="http://www.example.com/?class=WebServiceAuthApi&amp;wsdl" drupal_id="drupal_profile_id">
+  <drupal>
+    <endpoints>
+      <endpoint  auth_wsdl="http://www.example.com/?class=WebServiceAuthApi&amp;wsdl" drupal_id="drupal_profile_id"/>
+    </endpoints>
     <roles_map>
       <role access_zone="zone:my_drupal_role1" rid="9"/>
       <role access_zone="zone:my_drupal_role2" rid="5"/>
@@ -13,14 +16,23 @@ class YksAuthDrupal {
   static function login($user_login, $user_pswd) {
         //config map
     $drupal_config = yks::$get->config->drupal;
-
+    
     $roles_map = array();
     foreach($drupal_config->roles_map->iterate('role') as $role)
         $roles_map[$role['access_zone']] = $role['rid'];
-    $ws = new  SerializedSoapClient($drupal_config['auth_wsdl']);
 
-        //exyks login
-    $user_infos = $ws->login($user_login, $user_pswd);
+    foreach($drupal_config->endpoints->endpoint as $endpoint) {
+        try {
+            error_log("loop");
+            $ws = new  SerializedSoapClient($endpoint['auth_wsdl']);
+                //exyks login
+
+
+            $user_infos = $ws->login($user_login, $user_pswd);
+
+            if($user_infos) break;
+        }catch(Exception $e){}
+    }
 
         //second chance
     if(!$user_infos && bool($drupal_config['allow_fallback'])) {
@@ -38,17 +50,17 @@ class YksAuthDrupal {
     $access = $ws->getAccesses();
     $roles  = array_values(array_intersect_key($roles_map, $access));
 
-    $uid = $user_infos[$drupal_config['drupal_id']];
+    $uid = $user_infos[$endpoint['drupal_id']];
 
         //creation d'un drupal id & rattachement au profil existant
-    if($roles && !$uid) {
+    if(!$uid) {
         $data = array(
             'name'     => $user_infos['user_name'],
             'created'  => _NOW,
             'status'   => 1,
             'timezone' => 7200,
         ); $uid = sql::insert("users", $data, true);
-        $ws->update($drupal_config['drupal_id'], $uid);
+        $ws->update($endpoint['drupal_id'], $uid);
     }
 
 
