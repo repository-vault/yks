6576aa87778eb4ac73e71cc93378cfaf3248d37b
François Leurent <fleurent@ivsweb.com>
2011-01-05 13:20:29 +0000

    Finalement non
    git-svn-id: http://svn.exyks.org/yks/trunk@997 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/exyks/exyks.php
+++ class/exyks/exyks.php
@@ -21,7 +21,6 @@ class exyks {
   
 
   static function init() {
-    global $action;
 
     if(!defined('YKS_FUNCTIONS'))
       require CLASS_PATH."/functions.php";
@@ -51,7 +50,7 @@ class exyks {
     define('USERS_ROOT',     (int)yks::$get->config->users['root']);
     self::store('USERS_ROOT', USERS_ROOT); //drop constants here
 
-    $action   = (string)is_array($_POST['ks_action'])?key($_POST['ks_action']):$_POST['ks_action'];
+        
 
     self::$modules_list = array();
     if(!SITE_STANDALONE) 
@@ -68,6 +67,10 @@ class exyks {
 
 
   public static function web_init(){
+
+    global $action;
+
+    $action   = (string)is_array($_POST['ks_action'])?key($_POST['ks_action']):$_POST['ks_action'];
 
     $tmp = (string)yks::$get->config->site['default_mode'];
     define('DEFAULT_MODE', $tmp?$tmp:"xml");
