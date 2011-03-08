6142a5be4d5d1d66b1632856c6055bf9b8e6eaa7
François Leurent <fleurent@ivsweb.com>
2011-03-08 22:14:09 +0000

    BBcoder:extendable
    git-svn-id: http://svn.exyks.org/yks/trunk@1057 b8ca103b-dd03-488c-9448-c80b36131af2

--- rsrcs/js/Yks/Patchs/Native/Function.js
+++ rsrcs/js/Yks/Patchs/Native/Function.js
@@ -5,5 +5,12 @@ Function.implement({
         var tmp = args.extend($A(arguments));
         this.apply(thisArg, tmp);
     }.bind(this);
+  },
+
+ curry : function() {
+    var args = $A(arguments), method = this;
+    return function() {
+        return method.apply(this, args.concat($A(arguments)));
+    };
   }
 });
--- rsrcs/js/Yks/mts/Utilities/Rte/BBcoder.js
+++ rsrcs/js/Yks/mts/Utilities/Rte/BBcoder.js
@@ -1,11 +1,15 @@
 var BBcoder = new Class({
 
+  Binds:['focus', 'selectedText'],
+
   Declare : 'BBcoder',
   focus:function(){this.area.focus(); },
+  selectedText:function(){ return this.area.value.substring(this.pos.start, this.pos.end); },
 
   initialize:function(area, options){
     if(area.bbcoder) return false;
     this.options = options || {};
+    var actions = this.options.actions || BBcoder.actions;
 
     this.area = $(area);
     this.area.bbcoder = this;
@@ -21,13 +25,11 @@ var BBcoder = new Class({
         this.toolbar = this.options.toolbar;
     else this.toolbar = $n('div',{'class':'rte_toolbar topbar'}).inject(this.container,'top');
 
-
-    $H(this.actions).each(function(action, key){
-
+    $H(actions).each(function(callback, key){
         var div = new Element('div', {'class':key+' rte_button', 'unselectable':'on'});
         div.inject(this.toolbar);
         div.addEvent('mousedown', this.store.bind(this));
-        div.addEvent('mouseup', this.addtag.pass([key, action], this));
+        div.addEvent('mouseup', callback.bind(this));
    }.bind(this));
 
 
@@ -35,41 +37,50 @@ var BBcoder = new Class({
 
   store:function(){
     this.pos = this.area.getSelectedRange();
-    this.txt = this.area.getSelectedText();
-  },
-
-  addtag:function(type, action){
-    var pos = this.pos, selectedTxt = this.txt, txt = this.area.value ;
-
-    if(type=='hyperlink') {
-        if(selectedTxt.substring(0,7)=="http://")
-            action = ["[url="+selectedTxt+"]", action[1], action[2]];
-    }
-    this.area.value = txt.substring(0, pos.start) + action[0] + txt.substring(pos.start, pos.end) + action[1] + txt.substring(pos.end);
-    if(selectedTxt) 
-        this.area.selectRange(pos.start + (action[3]||0), pos.end + action[1].length + action[0].length + (action[4]||0));
-    else this.area.setCaretPosition(pos.start + action[2].length - action[1].length + (action[5] ||0));
-  },
-
-
-  actions:{
-    'picture'      :['[img]', '[/img]', '[img][/img]'],
-    'float_left'   :['[float=left]', '[/float]', '[float=left][/float]'],
-    'float_right'  :['[float=right]', '[/float]', '[float=right][/float]'],
-    'hyperlink'    :['[url=http://]', '[/url]', '[url][/url]', 0, 0, 7],
-    'underline'    :['[u]', '[/u]', '[u][/u]'],
-    'bold'         :['[b]', '[/b]', '[b][/b]'],
-    'italic'       :['[i]', '[/i]', '[i][/i]'],
-    'strike'       :['[strike]', '[/strike]', '[strike][/strike]'],
-    'left'         :['[left]', '[/left]', '[left][/left]'],
-    'right'        :['[right]', '[/right]', '[right][/right]'],
-    'center'       :['[center]', '[/center]', '[center][/center]'],
-    'justified'    :['[justify]', '[/justify]', '[justify][/justify]'],
-    'color'        :['[color=%s]', '[/color]', '[color=#][/color]'],
-    'hr'           :['', '[hr/]', '[hr/]', 0, 0, 5],
-    'small'        :['[size=+1]', '[/size]', '[size=][/size]'],
-    'big'          :['[size=-1]', '[/size]', '[size=][/size]']
   }
 
 
 });
+
+
+
+
+BBcoder.extend({
+ addtag:function(type, start, end, mid){
+    var pos = this.pos, txt = this.area.value;
+    if(pos.start != pos.end) {
+        this.area.value = txt.substring(0, pos.start) + start + txt.substring(pos.start, pos.end) + end + txt.substring(pos.end);
+        this.area.selectRange(pos.start, pos.end + end.length + start.length);
+    }  else {
+        this.area.value = txt.substring(0, pos.start) + mid + txt.substring(pos.end)
+        this.area.setCaretPosition(pos.start + mid.length - end.length );
+    }
+  }
+});
+
+BBcoder.extend({
+  actions:{
+    'picture'      : BBcoder.addtag.curry('picture', '[img]', '[/img]', '[img][/img]'),
+    'float_left'   : BBcoder.addtag.curry('float_left', '[float=left]', '[/float]', '[float=left][/float]'),
+    'float_right'  : BBcoder.addtag.curry('float_right', '[float=right]', '[/float]', '[float=right][/float]'),
+    'hyperlink'    : function(){
+        //yeah
+        if(this.selectedText().substring(0,7)=="http://")
+            BBcoder.addtag.call(this, 'hyperlink', '[url='+this.selectedText()+']', '[/url]', '[url][/url]');
+        else BBcoder.addtag.call(this, 'hyperlink', '[url=http://]', '[/url]', '[url][/url]')
+    },
+    'underline'    : BBcoder.addtag.curry('underline', '[u]', '[/u]', '[u][/u]'),
+    'bold'         : BBcoder.addtag.curry('bold', '[b]', '[/b]', '[b][/b]'),
+    'italic'       : BBcoder.addtag.curry('italic', '[i]', '[/i]', '[i][/i]'),
+    'strike'       : BBcoder.addtag.curry('strike', '[strike]', '[/strike]', '[strike][/strike]'),
+    'left'         : BBcoder.addtag.curry('left', '[left]', '[/left]', '[left][/left]'),
+    'right'        : BBcoder.addtag.curry('right', '[right]', '[/right]', '[right][/right]'),
+    'center'       : BBcoder.addtag.curry('center', '[center]', '[/center]', '[center][/center]'),
+    'justified'    : BBcoder.addtag.curry('justified', '[justify]', '[/justify]', '[justify][/justify]'),
+    'color'        : BBcoder.addtag.curry('color', '[color=%s]', '[/color]', '[color=#][/color]'),
+    'hr'           : BBcoder.addtag.curry('hr', '', '[hr/]', '[hr/]'),
+    'small'        : BBcoder.addtag.curry('small', '[size=+1]', '[/size]', '[size=][/size]'),
+    'big'          : BBcoder.addtag.curry('big', '[size=-1]', '[/size]', '[size=][/size]')
+  }
+});
+
