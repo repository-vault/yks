e2dd793b20f1ad7b1050869f7004a829fca62f50
François Leurent <fleurent@ivsweb.com>
2011-05-03 13:35:10 +0000

    If loaded iconv
    git-svn-id: http://svn.exyks.org/yks/trunk@1123 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/exts/iconv.php
+++ class/exts/iconv.php
@@ -3,11 +3,15 @@
 //This emulate iconv functions 
 // !!!! REQUIRE ext mb_string
 
-function iconv($in_charset, $out_charset, $str){
+if(!extension_loaded('iconv')) {
+  function iconv($in_charset, $out_charset, $str){
     $out_charset=preg_replace('#//[a-z]+$#i','',$out_charset);
     if($in_charset) return mb_convert_encoding( $str, $out_charset,$in_charset);
     return mb_convert_encoding( $str, $out_charset);
-}
-function iconv_strlen($str, $charset){
+  }
+
+  function iconv_strlen($str, $charset){
     return mb_strlen($str,$charset);
+  }
+
 }
