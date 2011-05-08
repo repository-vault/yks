1f3d48c33f422edf014bcbc8960e09fe4240c910
François Leurent <fleurent@ivsweb.com>
2011-05-08 00:10:19 +0000

    Slice proxy.php in abstract aserver (http server class) + new server.php (classic web server)
    git-svn-id: http://svn.exyks.org/exts/http/libs@232 628ac6e6-79fc-11de-858e-113bbc11c6a4

--- proxy.php
+++ proxy.php
@@ -1,32 +1,13 @@
 <?
+
 /* 131's
 *  http://doc.exyks.org/wiki/Source:ext/http/proxy
 */
 
-class http_proxy {
-  private $ip;
-  private $client_bind;
-  private $port;  
-  private $server;
-
-  protected $trace        = true;
-    //use binding on connect mode
-  protected $bind_on_ssl  = false;
+class http_proxy extends http_server {
 
   protected $auths  = array();
   protected $exclude_dests  = array();
-
-  function __construct($ip = '0.0.0.0', $port = '8080'){
-    $bind = "tcp://$ip:$port";
-  
-    $this->server = stream_socket_server($bind, $errno, $errstr);
-    if (!$this->server) 
-      throw new Exception ("$errstr ($errno)");
-  }
-
-  function close(){
-    fclose($this->server);
-  }
 
   function allow_account($login, $password){
     $this->auths['accounts'][$login] = $password;
@@ -50,44 +31,11 @@ class http_proxy {
     ); $this->stream_context = stream_context_create($opts);
   }
 
-  function run(){
-    $this->wait_for_clients();
-  }
-
-  private function wait_for_clients(){
-    if($this->trace) echo "Waiting for clients \r\n";
-    $serve_client = extension_loaded('pcntl') ? "fork_client" : "serve_client";
-    //$serve_client = "serve_client";
-
-    do {
-      $client = @stream_socket_accept($this->server);
-      if($client)
-         $this->$serve_client($client);
-    } while(true);
-    $this->close();
-  }
-
-
-  private function fork_client($client){
-    $pid = pcntl_fork();
-    if ($pid == -1) {
-         die('could not fork');
-         return;
-    } else if ($pid) {
-        pcntl_wait($c_status,WNOHANG);
-        fclose($client);
-        return; //waitagain
-
-    } else {
-        $this->serve_client($client);
-        die;
-    }
-  }
-
 /****************************** ***********************/
 
   protected function serve_client($client_stream){
-    list($method, $url_infos, $request_str) = $this->read_request($client_stream);
+
+    list($method, $url_infos, $request_str) = parent::serve_client($client_stream);
 
     if(!$method)
       return fclose($client);
@@ -102,19 +50,9 @@ class http_proxy {
     }
   }
 
-
   protected function read_request($client_stream) {
-    $request_str = $this->read_headers($client_stream);
-    if($this->trace) echo $request_str;
-    $mask = '#^([A-Z]+)\s*(.*?)\s*HTTP/[0-9]\.[0-9]\r\n#';
-    if(!preg_match($mask, $request_str, $out))
-        return null;
 
-      //drop the request first line
-    $request_str = substr($request_str, strlen($out[0]));
-
-    list(, $method, $url, $version) = $out;
-    $url_infos = parse_url($url);
+    list($method, $url_infos, $request_str) = parent::read_request($client_stream);
 
     if($this->auths)
       $this->proxy_authenticate($client_stream, $request_str);
@@ -198,9 +136,6 @@ class http_proxy {
     $this->tunnel_streams($dest_stream, $client_stream);
   }
 
-  protected function serve_std($dest_stream, $client_stream){
-      $this->stream_copy_to_stream($dest_stream, $client_stream);
-  }
 
   /******** Utils : internals ***************************/
 
@@ -223,54 +158,6 @@ class http_proxy {
     } else $dest = fsockopen($dest_host, $dest_port);
 
     return $dest;
-  }
-
-
-
-  /******** Utils : statics ***************************/
-
- protected function read_headers($lnk){
-    $headers_str = "";
-    do {
-      $line = fgets($lnk);
-      $headers_str .= $line;
-    } while($line != CRLF && $lnk);
-    return $headers_str;
-   }
-
-
-  protected function stream_copy_to_stream($src, $dest){
-    while($tmp=fread($src,1024)) fwrite($dest, $tmp);
-  }
-
-  protected function stream_copy_to_stream_nb($src, $dst, $content_length){
-    $sent_bytes = 0;
-    stream_set_blocking($src, 0);
-    while($sent_bytes< $content_length) {
-      $read = array($src);
-      $res = stream_select($read, $null, $null, null);
-      if($res === false) break;
-      if(feof($src) || feof($dst)) break;
-      $sent_bytes += fwrite($dst, fread($src, 8193));
-    }
-  }
-
-  protected function tunnel_streams($src, $dst){
-
-    do {
-      $read = array($src, $dst);
-      $res = stream_select($read, $null, $null, NULL);
-      if($res === false) break;
-      //if($res === 0) continue; //should not append
-      if(in_array($src, $read, true)) fwrite($dst, fread($src, 8193));
-      if(in_array($dst, $read, true)) fwrite($src, fread($dst, 8193));
-
-      if(feof($src) || feof($dst)) break;
-
-    } while (true);
-    fclose($src);
-    fclose($dst);
-
   }
 
 
