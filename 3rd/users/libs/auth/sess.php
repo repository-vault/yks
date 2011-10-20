094faf9480836fe9b450a68e4d656e1ad173ebdf
François Leurent <fleurent@ivsweb.com>
2011-10-20 12:16:38 +0000

    Default sess struct is ArrayObject
    git-svn-id: http://svn.exyks.org/yks/trunk@1217 b8ca103b-dd03-488c-9448-c80b36131af2

--- 3rd/users/libs/auth/sess.php
+++ 3rd/users/libs/auth/sess.php
@@ -54,7 +54,7 @@ class sess  {
 
   static function renew(){
     setcookie('user_id', false, 0, "/", SESS_DOMAIN);
-    sess::$sess     = array();
+    sess::$sess     = new ArrayObject();
     sess::$_storage = array();
     self::$renewed  = true;
     $sess_infos     = auth::valid_tree(exyks::retrieve('USERS_ROOT'));
