6bf818dd3fce35985418fd1022e93e69f469580c
François Leurent <fleurent@ivsweb.com>
2012-02-24 00:13:11 +0000

    win32_cli dispatcher
    git-svn-id: http://svn.exyks.org/exts/cli/trunk@252 628ac6e6-79fc-11de-858e-113bbc11c6a4

---
+++ win32_cli.php
@@ -0,0 +1,59 @@
+<?
+
+class win32_cli {
+
+  public static function dispatch(){
+    
+    $script = strip_end(basename(array_shift($_SERVER['argv'])), ".phpx");
+    $cb = array(__CLASS__, $script);
+
+    if(is_callable($cb)) 
+      return call_user_func_array($cb, $_SERVER['argv']);
+
+    rbx::error("No defined callback");
+
+  }
+  private static function getopt($params){
+    return cli::getopt($params, $_SERVER['argv']);
+  }
+
+  public static function wget($url){
+    list($options, $args) = self::getopt(array(
+          "O:"  => "output-document:",
+          "q"   => "quiet",
+    ));
+    $url   = $args[0]; $infos = parse_url($url);
+    $output = pick($options['O'], $options['output-document'], basename($infos['path']), "index.htm");
+
+    if($output == "-")
+      echo file_get_contents($url);
+    else {
+      copy($url, $output);
+      rbx::ok("Wrote in $output");
+    }
+  }
+
+  public static function md5file($file_path){
+    echo md5_file($file_path);
+  }
+
+  public static function which($file_path){
+    echo cli::which($file_path);
+  }
+
+  public static function touch($file_path){
+    touch($file_path, time());
+  }
+
+  public static function crypt($string, $key){
+    echo crypt::encrypt($string, $key, true);
+  }
+
+  public static function decrypt($string, $key){
+    echo crypt::decrypt($string, $key, true);
+  }
+
+
+
+
+}
