0f3bde19dbc11a42adfaec0b27c24b780f0b34ee
François Leurent <fleurent@ivsweb.com>
2012-06-01 10:53:06 +0000

    Test create closure
    git-svn-id: http://svn.exyks.org/yks/trunk@1325 b8ca103b-dd03-488c-9448-c80b36131af2

---
+++ tests/closures/test.php
@@ -0,0 +1,21 @@
+<?
+include 'yks/cli.php';
+
+class msg {
+  function say($msg){
+    echo "Say : $msg\r\n";
+ }
+} $msg = new msg();
+
+$this_contents = file_get_contents("this.txt");
+$that_contents = file_get_contents("that.txt");
+
+
+$func1  = create_function('$that', $that_contents);
+$func3  = create_closure('$that', $that_contents);
+$func2  = create_closure('', $this_contents);
+
+call_user_func_array($func1, array($msg));
+call_user_func_array($func3, array($msg));
+call_user_func($func2->bindTo($msg));
+
