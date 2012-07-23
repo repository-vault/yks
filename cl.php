c9175f18a54362ddc4b05a2e87c261909dcc39d2
François Leurent <fleurent@ivsweb.com>
2012-07-23 14:31:48 +0000

    Define clyks config in ... config
    git-svn-id: http://svn.exyks.org/yks/trunk@1374 b8ca103b-dd03-488c-9448-c80b36131af2

--- cl.php
+++ cl.php
@@ -13,12 +13,19 @@ define("PUBLIC_PATH", getcwd()."/www");
 include "yks/class/yks/loader.php";
 exyks::init();
 
+$clyks_config = yks::$get->config->clyks;
+
 $z_e_r_o_entries = array(
   'yks'  => 'yks_runner',
   'myks' => 'myks_runner',
   'sync' => 'sync_runner',
   'sql'  => 'sql_runner',
-); $entry = $z_e_r_o_entries[pick_in($entry, array_keys($z_e_r_o_entries))];
+);
+if($bootstrap = $clyks_config->bootstrap['class']){
+  $z_e_r_o_entries = array_merge(array($bootstrap=>$bootstrap), $z_e_r_o_entries);
+}
+
+$entry = $z_e_r_o_entries[pick_in($entry, array_keys($z_e_r_o_entries))];
 
 
 
