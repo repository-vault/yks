eac70c45a7d5325b6064c4ffd813d33771432044
François Leurent <fleurent@ivsweb.com>
2012-10-10 10:57:59 +0000

    YKS : Fix pour sendAsBinary pour Chrome.
    git-svn-id: http://svn.exyks.org/yks/trunk@1450 b8ca103b-dd03-488c-9448-c80b36131af2

--- rsrcs/js/Yks/libs/Xhr.js
+++ rsrcs/js/Yks/libs/Xhr.js
@@ -97,5 +97,13 @@ var Xhr = new Class({
 
 });
 
-
-
+if(Browser.Engine.name == 'webkit'){
+  XMLHttpRequest.prototype.sendAsBinary = function(datastr) {
+    function byteValue(x) {
+      return x.charCodeAt(0) & 0xff;
+    }
+    var ords = Array.prototype.map.call(datastr, byteValue);
+    var ui8a = new Uint8Array(ords);
+    this.send(ui8a);
+  }
+}
