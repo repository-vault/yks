8461ab1cbccc1d0eb630af2bd49422cef46964da
François Leurent <fleurent@ivsweb.com>
2013-01-18 11:28:30 +0000

    Yks : Utilisation des sessions php pour les appels aux WebServices
    git-svn-id: http://svn.exyks.org/yks/trunk@1535 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/apis/drupal/AuthDrupal.php
+++ class/apis/drupal/AuthDrupal.php
@@ -1,84 +1,77 @@
-<?
-/** Client side 
-  <drupal>
-    <endpoints>
-      <endpoint  auth_wsdl="http://www.example.com/?class=WebServiceAuthApi&amp;wsdl" drupal_id="drupal_profile_id"/>
-    </endpoints>
-    <roles_map>
-      <role access_zone="zone:my_drupal_role1" rid="9"/>
-      <role access_zone="zone:my_drupal_role2" rid="5"/>
-    </roles_map>
-  </drupal>
+<?php
+/*
+<endpoints>
+<endpoint  auth_wsdl="http://www.example.com/?class=WebServiceDrupalAuthApi&amp;wsdl" drupal_id="drupal_profile_id" domain=""/>
+</endpoints>
+<roles_map>
+<role access_zone="zone:my_drupal_role1" rid="9"/>
+<role access_zone="zone:my_drupal_role2" rid="5"/>
+</roles_map>
+</drupal>
 **/
 
 class YksAuthDrupal {
 
+  protected static $session_id;
+
   static function login($user_login, $user_pswd) {
-        //config map
+    //config map
     $drupal_config = yks::$get->config->drupal;
-    
+
     $roles_map = array();
     foreach($drupal_config->roles_map->iterate('role') as $role)
-        $roles_map[$role['access_zone']] = $role['rid'];
-
-    foreach($drupal_config->endpoints->endpoint as $endpoint) {
-        try {
-            $ws = new  SerializedSoapClient($endpoint['auth_wsdl']);
-                //exyks login
-            $user_infos = $ws->login($user_login, $user_pswd);
-
-            if($user_infos) break;
-        }catch(Exception $e){}
+      $roles_map[$role['access_zone']] = $role['rid'];
+      
+    foreach($drupal_config->endpoints->endpoint as $endpoint){
+      try {
+        $ws = new  SoapClient($endpoint['auth_wsdl']);
+        //exyks login
+        self::$session_id = $ws->login($user_login, $user_pswd, $endpoint['domain']);
+        $user_infos = unserialize($ws->getUser(self::$session_id));
+      }catch(Exception $e){}
     }
 
-        //second chance
+    //second chance
     if(!$user_infos && bool($drupal_config['allow_fallback'])) {
-        $verif_account = array(
-            'name' => $user_login,
-            'pass' => md5($user_pswd)
-        ); $uid = sql::value("users", $verif_account, "uid");
-        return array($uid);
+      $verif_account = array(
+        'name' => $user_login,
+        'pass' => md5($user_pswd)
+      ); $uid = sql::value("users", $verif_account, "uid");
+      return array($uid);
     }
-
 
     if(!$user_infos)
-        throw new Exception("Invalid login/pswd");
+      throw new Exception("Invalid login/pswd");
 
-    $access = $ws->getAccesses();
+    $access = unserialize($ws->getAccesses(self::$session_id));
     $roles  = array_values(array_intersect_key($roles_map, $access));
-
     $uid = $user_infos[$endpoint['drupal_id']];
 
-        //creation d'un drupal id & rattachement au profil existant
+    //creation d'un drupal id & rattachement au profil existant
     if(!$uid) {
-        $data = array(
-            'name'     => $user_infos['user_name'],
-            'created'  => _NOW,
-            'status'   => 1,
-            'timezone' => 7200,
-        ); $uid = sql::insert("users", $data, true);
-        $ws->update($endpoint['drupal_id'], $uid);
+      $data = array(
+        'name'     => $user_infos['user_name'],
+        'created'  => _NOW,
+        'status'   => 1,
+        'timezone' => 7200,
+      ); $uid = sql::insert("users", $data, true);
+      $ws->update(self::$session_id, $endpoint['drupal_id'], $uid);
     }
-
-
-
-    if(!$uid) 
-        throw new Exception("Invalid drupal id");
-
-
+    if(!$uid)
+      throw new Exception("Invalid drupal id");
     $verif_user = compact('uid');
 
-        //update drupal's profile
+    //update drupal's profile
     $data = array(
-        'name' => $user_infos['user_name'],
-        'mail' => $user_infos['user_mail'],
-        'pass' => null,
+      'name' => $user_infos['user_name'],
+      'mail' => $user_infos['user_mail'],
+      'pass' => null,
     ); sql::update("users", $data, $verif_user);
 
-        //update drupal's roles
+    //update drupal's roles
     sql::delete("users_roles",  $verif_user);
     foreach($roles as $rid)
-        sql::insert("users_roles", compact('uid', 'rid'));
+      sql::insert("users_roles", compact('uid', 'rid'));
 
     return array($uid);
   }
