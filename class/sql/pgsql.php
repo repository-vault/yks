67ed17045e29315f32c7bf8fbc2752c47f72f3d3
François Leurent <fleurent@ivsweb.com>
2013-01-28 23:10:39 +0000

    dont escape ',' but fields name ....
    git-svn-id: http://svn.exyks.org/yks/trunk@1553 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/sql/pgsql.php
+++ class/sql/pgsql.php
@@ -157,7 +157,7 @@ class sql {
       return "SET ".mask_join(',', array_key_map(array('sql', 'escape'), $vals), '%2$s = %1$s');
     }
 
-    return "(".self::escape( join( self::escape(','), array_keys($vals))  ) .") VALUES(" . join(',', $vals) . ")";
+    return "(". join( ',', array_map( array('sql', 'escape'), array_keys($vals))  ) .") VALUES(" . join(',', $vals) . ")";
   }
 
   static function close($lnk = false) {
