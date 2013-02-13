1903fce1d17b300d5b1ebb0330bd7176886ceda5
François Leurent <fleurent@ivsweb.com>
2013-02-13 23:41:48 +0000

    Expose seek
    git-svn-id: http://svn.exyks.org/yks/trunk@1573 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/cli/tail.php
+++ class/cli/tail.php
@@ -1,5 +1,6 @@
 <?
 
+
 class cli_tail {
   private $file_path = '';
 
@@ -7,20 +8,34 @@ class cli_tail {
     $this->file_path = $file_path;
     $this->fp = fopen($this->file_path, "r");
     $block = 4096;
-    $this->position = 0;
+    $this->seek(0);
   }
 
-  public function pick_line() {
+  public function seek($position){
+    $this->position = $position;
+  }
+
+  function pick_line() {
         clearstatcache ();
       if($this->position == filesize($this->file_path))
         return;
       if($this->position > filesize($this->file_path))
         $this->position = filesize($this->file_path);
+
       fseek($this->fp, $this->position, SEEK_SET);
       $line = fgets($this->fp);
       
       $this->position = ftell($this->fp);
       return $line;
+  }
+
+  public function wait_line($timeout = 1) {
+    $timeout = timeout::from_seconds($timeout);
+    do {
+        $line = $this->pick_line();
+        if($line) return $line;
+        $timeout->sleep(0.3);
+    } while(!$timeout->expired);
   }
 
   public function tail($lines = 10)  {
