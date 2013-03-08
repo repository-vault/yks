3e45e8a2786a56a7135b3aec8ae3b473c1af24a9
François Leurent <fleurent@ivsweb.com>
2013-03-08 15:00:43 +0000

    MediaWiki: Authorize all permissions for every namespace group users.
    git-svn-id: http://svn.exyks.org/yks/trunk@1609 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/apis/mediawiki/AuthDomyks.php
+++ class/apis/mediawiki/AuthDomyks.php
@@ -78,23 +78,16 @@ class Auth_Domyks extends AuthPlugin {
   public function addNamespace($name, $base_id){
     global $wgExtraNamespaces;
     global $wgNamespacePermissionLockdown;
-    global $wgNamespaceProtection;
     global $wgContentNamespaces;
 
     //Create namespace & talk
     $wgExtraNamespaces[$base_id] = $name;
     $wgExtraNamespaces[$base_id+1] = "{$name}_talk";
 
-    //Base protection
-    $wgNamespaceProtection[$base_id] = array($name);
-    $wgNamespaceProtection[$base_id+1] = array($name);
-
     //Lockdown protection
-    $wgNamespacePermissionLockdown[$base_id]['read'] = array($name);
-    $wgNamespacePermissionLockdown[$base_id]['edit'] = array($name);
-    $wgNamespacePermissionLockdown[$base_id+1]['read'] = array($name);
-    $wgNamespacePermissionLockdown[$base_id+1]['edit'] = array($name);
-    
+    $wgNamespacePermissionLockdown[$base_id]['*'] = array($name);
+    $wgNamespacePermissionLockdown[$base_id+1]['*'] = array($name);
+
     //Add namepace to content
     $wgContentNamespaces[] = $base_id;
   }
