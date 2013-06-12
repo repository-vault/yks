3b116d2f522b2a5e65bfb594b39bc2cf9970b008
François Leurent <fleurent@ivsweb.com>
2013-06-12 14:25:40 +0000

    Yks : add config for mime.type path
    git-svn-id: http://svn.exyks.org/yks/trunk@1687 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/stds/mime/types.php
+++ class/stds/mime/types.php
@@ -5,6 +5,7 @@ class mime_types {
   private static $mime_types = null;
   static function init(){
     self::$mime_path = $_SERVER['mime_types_path'];
+    if(!self::$mime_path) self::$mime_path = yks::$get->config->paths->mime['path'];
     if(!self::$mime_path) self::$mime_path = "/etc/httpd/conf/mime.types";
     if(!is_file(self::$mime_path))
         throw new Exception("mime_type, invalid configuration, chk env mime_types_path");
@@ -28,10 +29,7 @@ class mime_types {
   static function get_ext($content_type){
     if(is_null(self::$mime_types))
       self::build_cache();
-
-    $tmp = array_flip(self::$mime_types);
-
-    return $tmp[$content_type];
+    return array_search($content_type, self::$mime_types);
   }
 
   static function get_content_type($ext){
