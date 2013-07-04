024654589cbfe937a082bf09cabcbf622f1e9989
François Leurent <fleurent@ivsweb.com>
2013-07-04 11:56:57 +0000

    Enhance storage_redis driver
    git-svn-id: http://svn.exyks.org/yks/trunk@1700 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/stds/storage/redis.php
+++ class/stds/storage/redis.php
@@ -4,35 +4,60 @@
 *  @alias storage
 **/
 class _storage_redis {
-  
+
   private static $redis = null;
-  
-  public static function init(){
+  private static $local_cache = array();
+
+  public static function __construct_static(){
     if(self::$redis != null)
       return;
-    
-    //Connect to redis
+
     $conf = yks::$get->config->storage;
-    $host = pick(trim($conf['host']), '127.0.0.1');
-    $port = pick((int)$conf['port'], 6379);
+
+    if(trim($conf['driver']) != 'redis')
+      throw new Exception("Invalid redis configuration '{$conf['driver']}'");
+
+    //Get list of redis
+    $hosts_redis = array();
+    foreach($conf->redis as $r){
+      $hosts_redis[trim($r['name'])] = array(
+        'host' => trim($r['host']),
+        'port' => (int)$r['port'],
+      );
+    }
+
+    $host_redis = $hosts_redis[pick( $conf['default_lnk'], "default") ];
+
     self::$redis = new Redis();
-    if(!self::$redis->connect($host, $port))
-      throw new Exception("Failed to connect on redis host $host:$port");
+    if(!self::$redis->connect($host_redis['host'], $host_redis['port'])){
+      self::$redis = false;
+      syslog(LOG_ERR, "Failed to connect to redis host, disabling cache");
+    }
   }
-  
+
+
   static function store($k, $v, $ttl=0){
-    return self::$redis->set($k, serialize($v), $ttl);
+    if(!self::$redis )
+      return self::$local_cache[$k] = $v;
+    return self::$redis->set($k, serialize($v), $ttl) ;
   }
 
   static function fetch($k){
+    if(!self::$redis)
+     return self::$local_cache[$k];  
+
     $out = self::$redis->get($k);
     return $out ? unserialize($out) : false;
   }
 
   static function delete($k){
+    if(!self::$redis)
+     return unset(self::$local_cache[$k]);
+
     return self::$redis->del($k);
   }
-  
+
+
   public static function clean(){
     return false;
   }
