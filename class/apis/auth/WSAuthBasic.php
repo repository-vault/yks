cd8b93d6fdb6ea0c8c7add7e502be4488ee99866
François Leurent <fleurent@ivsweb.com>
2013-07-18 00:22:44 +0000

    Proper indent
    git-svn-id: http://svn.exyks.org/yks/trunk@1717 b8ca103b-dd03-488c-9448-c80b36131af2

--- class/apis/auth/WSAuthBasic.php
+++ class/apis/auth/WSAuthBasic.php
@@ -1,68 +1,68 @@
 <?php
 
-  class WSAuthBasic {
+class WSAuthBasic {
 
-    const TRUE_CLASS = 'exyks_auth_api';
+  const TRUE_CLASS = 'exyks_auth_api';
 
-    /**
-    * Retourne la session id
-    * @param string $user_login
-    * @param string $user_pswd
-    * @return string
-    **/
-    public static function login($user_login, $user_pswd){
-      try{
-        $args = func_get_args();
-        call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
-        return session_id();
-      }
-      catch(Exception $e){
-        Throw new SoapFault( $e->getMessage(), $e->getMessage());
-      }
-    }
-
-    /**
-    * Login with an ID. Restricted by IP for internals exchanges
-    *
-    * @param int $user_id
-    * @return string
-    */
-    public static function loginFromId($user_id){
+  /**
+  * Retourne la session id
+  * @param string $user_login
+  * @param string $user_pswd
+  * @return string
+  **/
+  public static function login($user_login, $user_pswd){
+    try{
       $args = func_get_args();
-      return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+      call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+      return session_id();
     }
-
-    /**
-    * Retourne un object serialise definissant l'user
-    * @param string $session_id
-    * @return string
-    **/
-    public static function getUser($session_id){
-      $args = func_get_args(); array_shift($args);
-      return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+    catch(Exception $e){
+      Throw new SoapFault( $e->getMessage(), $e->getMessage());
     }
-
-    /**
-    * Valide un niveau d'access
-    * @param string $session_id
-    * @param string $access_zone
-    * @param string $access_lvl
-    * @return boolean
-    **/
-    public function verifAuth($session_id, $access_zone, $access_lvl){
-      $args = func_get_args(); array_shift($args);
-      return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
-    }
-
-    /**
-    * Valide un niveau d'access
-    * @param string $session_id
-    * @return string
-    **/
-    public function getAccesses($session_id){
-      $args = func_get_args();
-      return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
-    }
-
-
   }
+
+  /**
+  * Login with an ID. Restricted by IP for internals exchanges
+  *
+  * @param int $user_id
+  * @return string
+  */
+  public static function loginFromId($user_id){
+    $args = func_get_args();
+    return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+  }
+
+  /**
+  * Retourne un object serialise definissant l'user
+  * @param string $session_id
+  * @return string
+  **/
+  public static function getUser($session_id){
+    $args = func_get_args(); array_shift($args);
+    return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+  }
+
+  /**
+  * Valide un niveau d'access
+  * @param string $session_id
+  * @param string $access_zone
+  * @param string $access_lvl
+  * @return boolean
+  **/
+  public function verifAuth($session_id, $access_zone, $access_lvl){
+    $args = func_get_args(); array_shift($args);
+    return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+  }
+
+  /**
+  * Valide un niveau d'access
+  * @param string $session_id
+  * @return string
+  **/
+  public function getAccesses($session_id){
+    $args = func_get_args();
+    return call_user_func_array(array(self::TRUE_CLASS, __FUNCTION__), $args);
+  }
+
+
+}
--- class/apis/auth/WSAuthExternal.php
+++ class/apis/auth/WSAuthExternal.php
@@ -1,48 +1,48 @@
 <?php
 
-  class WSAuthExternal {
+class WSAuthExternal {
 
-    /**
-    * Connect a user using a google token
-    *
-    * @param string $token google token for email https://www.googleapis.com/auth/userinfo.email
-    *
-    * @return string session_id
-    */
-    public static function loginGoogleToken($token){
-        $infos = file_get_contents("https://www.googleapis.com/oauth2/v3/userinfo?access_token=".$token);
-        $infos = json_decode($infos, true);
-        if($infos['error']){
-          Throw new SoapFault('TokenError', 'Invalid google token');
-        }
+  /**
+  * Connect a user using a google token
+  *
+  * @param string $token google token for email https://www.googleapis.com/auth/userinfo.email
+  *
+  * @return string session_id
+  */
+  public static function loginGoogleToken($token){
+      $infos = file_get_contents("https://www.googleapis.com/oauth2/v3/userinfo?access_token=".$token);
+      $infos = json_decode($infos, true);
+      if($infos['error']){
+        Throw new SoapFault('TokenError', 'Invalid google token');
+      }
 
-        $domain_list = yks::$get->config->apis->authapi->googleauth->domains->children();
-        $domain_error = yks::$get->config->apis->authapi->googleauth->domains['error_msg'];
+      $domain_list = yks::$get->config->apis->authapi->googleauth->domains->children();
+      $domain_error = yks::$get->config->apis->authapi->googleauth->domains['error_msg'];
 
-        $avalaible_domain = array();
-        foreach($domain_list as $domain){
-          $avalaible_domain[] = (String)$domain;
-        }
+      $avalaible_domain = array();
+      foreach($domain_list as $domain){
+        $avalaible_domain[] = (String)$domain;
+      }
 
-        list($user , $domain) = explode('@', $infos['email'], 2);
+      list($user , $domain) = explode('@', $infos['email'], 2);
 
-        if(!in_array($domain, $avalaible_domain))
-          Throw new SoapFault('MailError', $infos['email'].' '.$domain_error);
+      if(!in_array($domain, $avalaible_domain))
+        Throw new SoapFault('MailError', $infos['email'].' '.$domain_error);
 
-        //IVS mail OK && active
-        $user_id = sql::value('ks_users_profile', array('user_mail' => $infos['email']), 'user_id');
+      //IVS mail OK && active
+      $user_id = sql::value('ks_users_profile', array('user_mail' => $infos['email']), 'user_id');
 
-        sess::connect();
-        if(!isset(sess::$sess['user_id']))
-          sess::renew(); //sess::$id is now set
+      sess::connect();
+      if(!isset(sess::$sess['user_id']))
+        sess::renew(); //sess::$id is now set
 
-        $auth_success = sess::update($user_id, true); //skip authentification
+      $auth_success = sess::update($user_id, true); //skip authentification
 
-        if(!$auth_success)
-         throw new SoapFault("BadUser", $infos['email']." is not valid user");
+      if(!$auth_success)
+       throw new SoapFault("BadUser", $infos['email']." is not valid user");
 
 
-      return session_id();
-    }
-
+    return session_id();
   }
+
+}
