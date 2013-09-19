2c35416c1f3bf9b6a36d531cffecd9b3b3cd2e4d
François Leurent <fleurent@ivsweb.com>
2013-09-19 15:33:41 +0000

    ArrayObject --enhance : expose any object with ALL its properties (even privates)
    git-svn-id: http://svn.exyks.org/yks/trunk@1762 b8ca103b-dd03-488c-9448-c80b36131af2

--- libs/_arrayobject.php
+++ libs/_arrayobject.php
@@ -1,7 +1,35 @@
 <?
 class _ArrayObject implements ArrayAccess {
+
   function offsetExists ($key){ return isset($this->$key); }
   function offsetGet($key){ return $this->$key;}
   function offsetSet($key, $value){$this->$key = $value;}
   function offsetUnset($key){unset($this->$key); }
+
+
+  public static function expose($obj){
+    if(is_scalar($obj) || is_null($obj)) 
+      return $obj;
+
+    if(is_array($obj)) {
+      $out = new self();
+      foreach($obj as $k=>$v)  
+        $out->$k = call_user_func(__METHOD__, $v);
+      return $out;
+    }
+
+    if(is_object($obj)) {
+      $out = new self();
+      $fx = new ReflectionObject($obj);
+      foreach($fx->getProperties() as $property) {
+          $property_name = $property->getName();
+          $property->setAccessible(true);
+          $out->$property_name = call_user_func(__METHOD__, $property->getValue($obj));
+      }
+      return $out ;
+    }
+
+    return null;
+
+  }
 }
