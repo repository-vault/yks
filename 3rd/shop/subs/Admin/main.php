4a37d58a5063581cc0a5c4a0242d97eca7737259
François Leurent <fleurent@ivsweb.com>
2013-11-21 14:23:45 +0000

    Use new nav !
    git-svn-id: http://svn.exyks.org/yks/trunk@1813 b8ca103b-dd03-488c-9448-c80b36131af2

--- 3rd/shop/subs/Admin/main.php
+++ 3rd/shop/subs/Admin/main.php
@@ -4,17 +4,17 @@ auth::verif("shop", "admin", 403);
 auth::verif("shop", "action", true);
 
 
-tpls::$nav[$href_fold]['children']["$href_fold/Products"]=array(
+$vals[$href_fold]['children']["$href_fold/Products"] = array(
     'href'=>"/?$href_fold/Products",
     'title'=>'Produits',
     'target'=>'_top',
 );
-
-tpls::$nav[$href_fold]['children']["$href_fold/Meta list"]=array(
+$vals[$href_fold]['children']["$href_fold/Meta list"]=array(
     'href'=>"/?$href_fold/Meta",
     'title'=>'Meta',
     'target'=>'_top',
 );
+tpls::$nav->stack($vals);
 
 
 if($href == "$href_fold/") reloc("/?$href_fold/Orders");
