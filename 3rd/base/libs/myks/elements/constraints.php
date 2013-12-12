08294d3c3017985146a23852f85258ffcd0ce1eb
François Leurent <131.php@cloudyks.org>
2013-12-12 23:45:53 +0100

    - Drop useless constraint of type "INDEX" references - new defered constraint check timing : 'best' => 'defer' for 'foreign', 'strict' for unique & primary (usefull for 8.2 compat) - allow field @constraint=unique syntax

--- 3rd/base/libs/myks/elements/constraints.php
+++ 3rd/base/libs/myks/elements/constraints.php
@@ -5,7 +5,7 @@ abstract class myks_constraints_base {
   private  $constraints_xml;
 
   protected $escape_char="`";
-  public $key_mask=array("PRIMARY"=>'PRIMARY KEY',  "INDEX" => "INDEX", "UNIQUE"=>'UNIQUE', 'FOREIGN'=>'FOREIGN KEY' );
+  public $key_mask=array("PRIMARY"=>'PRIMARY KEY',  "UNIQUE"=>'UNIQUE', 'FOREIGN'=>'FOREIGN KEY' );
 
   protected $parent;
 
@@ -31,6 +31,9 @@ abstract class myks_constraints_base {
   public function key_add($type, $member, $refs=array()){
     $TYPE = strtoupper($type);
 
+    if($TYPE == "PRIMARY")
+        $refs['defer'] = 'strict'; //primary keys cannot be defered
+
     $key_name = sprintf($this->keys_name[$TYPE], $this->parent->name['name'], $member, $type);
     $key_name = substr($key_name, 0, 63);
     $this->key_stack($type, array($member => $member), $key_name, $refs);
@@ -41,6 +44,10 @@ abstract class myks_constraints_base {
     $TYPE=strtoupper($type);
     if(!isset($this->xml_def[$key_name]))
       $this->xml_def[$key_name] = array('members' => array());
+
+    if($refs['defer'] == 'best')
+        $refs['defer'] = ($TYPE == "FOREIGN") ? "defer" : "strict"; //8.2 compat
+
 
     $this->xml_def[$key_name] = array_merge($this->xml_def[$key_name], $refs);
     $this->xml_def[$key_name]['type'] = $TYPE;
@@ -68,18 +75,23 @@ abstract class myks_constraints_base {
           $target_members[(string)$member['column']] = (string)$member['target'];;
         }
 
-        if(!($key_name = (string)$constraint_xml['name'])){
-
+        if(!($key_name = (string)$constraint_xml['name']))
           $key_name = "{$this->parent->name['name']}_".strtolower($type)."_$i";
-        }
 
-        $fk_name = sql::resolve((string)$constraint_xml['fk_table']);
+
         $refs = array(
-          "refs"     => self::build_ref($fk_name['schema'], $fk_name['name'], $target_members),
-          "update"   => (string)$constraint_xml['update'],
-          "delete"   => (string)$constraint_xml['delete'],
-          "defer"    => (string)$constraint_xml['defer'],
+            "defer"    => (string)$constraint_xml['defer']
         );
+
+        if($type == "FOREIGN") {
+          $fk_name = sql::resolve((string)$constraint_xml['fk_table']);
+           $refs = array_merge($refs, array(
+            "refs"     => self::build_ref($fk_name['schema'], $fk_name['name'], $target_members),
+            "update"   => (string)$constraint_xml['update'],
+            "delete"   => (string)$constraint_xml['delete'],
+          ));
+        }
+        
 
         $this->key_stack($type, $members, $key_name, $refs);
     }
@@ -96,7 +108,7 @@ abstract class myks_constraints_base {
   }
 
   function alter_def(){
-    //print_r(array_show_diff($this->sql_def, $this->xml_def,"sql","xml"));die;
+    //print_r(array_show_diff($this->sql_def, $this->xml_def,"sql","xml"));
 
     $ec = '"';
     $table_alter = "ALTER TABLE {$this->parent->name['safe']} ";
@@ -105,29 +117,25 @@ abstract class myks_constraints_base {
 
     foreach($this->sql_def as $key=>$def){
         if($this->xml_def[$key] != $def)
-            array_unshift($todo, $drop = "$table_alter DROP ".
-                (($def['type']=="PRIMARY" || $def['type']=="FOREIGN"|| $def['type']=="UNIQUE")?
-                    "CONSTRAINT {$ec}$key{$ec}"
-                    :"INDEX {$ec}$key{$ec}") );
+            array_unshift($todo, $drop = "$table_alter DROP CONSTRAINT {$ec}$key{$ec}" );
         else unset($this->xml_def[$key]);
     }
 
     foreach($this->xml_def as $key=>$def){
-        $members=' ("'.join('","',$def['members']).'")';
-        $type=strtoupper($def['type']);
-        $add = "ADD CONSTRAINT $key ".$this->key_mask[$type]." $members ";
-        if($type=="INDEX") {
-          $todo[]="CREATE INDEX $key ON {$this->table_name['safe']} $members";continue;
+        $members = ' ("'.join('","', $def['members']).'")';
+        $type    = strtoupper($def['type']);
+        $add     = "ADD CONSTRAINT $key ".$this->key_mask[$type]." $members ";
+
+        if($type == "FOREIGN"){
+            $add .= " REFERENCES ".self::output_ref($def['refs'])." ";
+            if($def['delete']) $add .= " ON DELETE ".self::$fk_actions_out[$def['delete']];
+            if($def['update']) $add .= " ON UPDATE ".self::$fk_actions_out[$def['update']];
         }
-        elseif($type=="UNIQUE") {
-            if($def['defer']=='defer') $add.=" DEFERRABLE INITIALLY DEFERRED";
-        }
-        elseif($type=="FOREIGN"){
-            $add.=" REFERENCES ".self::output_ref($def['refs'])." ";
-            if($def['delete']) $add.=" ON DELETE ".self::$fk_actions_out[$def['delete']];
-            if($def['update']) $add.=" ON UPDATE ".self::$fk_actions_out[$def['update']];
-            if($def['defer']=='defer') $add.=" DEFERRABLE INITIALLY DEFERRED";
-        } $todo[]="$table_alter $add";
+
+        if($def['defer']=='defer')
+            $add .= " DEFERRABLE INITIALLY DEFERRED";
+
+        $todo[] = "$table_alter $add";
     }
     return $todo;
   }
