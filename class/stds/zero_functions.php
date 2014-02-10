63201d12c4cf98f3aa531f33ca80bce791d840cb
François Leurent <131.php@cloudyks.org>
2014-02-10 10:59:36 +0100

    Strtolower guid / according to postgres (...)

--- class/stds/zero_functions.php
+++ class/stds/zero_functions.php
@@ -15,10 +15,10 @@ function create_closure($args, $body){
 
 
 function guid() {
-    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
+    return strtolower(sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X',
         mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535),
         mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535),
-        mt_rand(0, 65535), mt_rand(0, 65535));
+        mt_rand(0, 65535), mt_rand(0, 65535)));
 }
 
 function paths_merge($path_root, $path, $default="."){
