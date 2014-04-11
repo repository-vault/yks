908a35be3828eddf886292cff251a1c448f68ebc
Quentin Mansuy <qmansuy@ivsweb.com>
2014-04-11 17:27:27 +0200

    Geomap : add list, improve IHM.

--- 3rd/geomaps/libs/geomaps.php
+++ 3rd/geomaps/libs/geomaps.php
@@ -2,6 +2,11 @@
 
 class geomaps {
 
+  const sql_table = 'ks_users_geomaps_list';
+  const sql_key   = 'map_id';
+  protected $sql_table = self::sql_table;
+  protected $sql_key   = self::sql_key;
+
   private static $maps_path;
   protected $area_colors;
   protected $png_map;
@@ -12,14 +17,14 @@ class geomaps {
 
   public function __construct($map_id){
     $verif_map  = compact('map_id');
-    $this->data = sql::row("ks_users_geomaps_list", $verif_map);
+    $this->data = sql::row(self::sql_table, $verif_map);
     if(!$this->data)
         throw new Exception("Invalid map #");
 
     if(!is_file($this->data['map_path']))
         throw new Exception("Map $map_id file not ready");
     $this->png_map = new png_map($this->data['map_path']);
-    
+
   }
 
 
@@ -29,6 +34,11 @@ class geomaps {
         $this->png_map->set_color($hash_key, $color);
 
     $this->png_map->output();
+  }
+
+  public static function list_maps($where=array(sql::true)) {
+    sql::select(self::sql_table,$where);
+    return sql::brute_fetch(self::sql_key);
   }
 
 }
