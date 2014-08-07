f504a934da29aca414b486e86b097593c032a973
François Leurent <131.php@cloudyks.org>
2014-08-08 01:42:57 +0200

    yks-bootstrap : proper fonts exposure

--- 3rd/base/subs/Yks/Scripts/Contents/main.php
+++ 3rd/base/subs/Yks/Scripts/Contents/main.php
@@ -8,10 +8,37 @@ $full = exyks_paths::resolve_public($path);
 if(!is_file($full))
     die("No file here");
 
-define('TYPE_ICO', "Content-Type: image/x-icon");
+define('TYPE_ICO',  'Content-Type: image/x-icon');
+define('TYPE_GIF',  'Content-type: image/gif');
+define('TYPE_SVG',  'Content-Type: image/svg+xml');
+define('TYPE_TTF',  'Content-Type: application/x-font-ttf');
+define('TYPE_OTF',  'Content-Type: application/x-font-opentype');
+define('TYPE_WOFF', 'Content-Type: application/font-woff');
+define('TYPE_EOT',  'Content-Type: application/vnd.ms-fontobject');
 
 
-switch(files::ext($full)) {
+$dummies = array(
+    'png'  => TYPE_PNG,
+    'json' => TYPE_JSON,
+    'cur'  => TYPE_ICO,
+    'ico'  => TYPE_ICO,
+    'gif'  => TYPE_GIF,
+    'jpeg' => TYPE_JPEG,
+    'jpg'  => TYPE_JPEG,
+    'svg'  => TYPE_SVG,
+    'ttf'  => TYPE_TTF,
+    'otf'  => TYPE_OTF,
+    'woff' => TYPE_WOFF,
+    'eot'  => TYPE_EOT
+);
+
+$ext = files::ext($full);
+
+if(isset($dummies[$ext])) {
+    header($dummies[$ext]);
+    files::delivers($full);
+    die;
+} switch($ext) {
   case 'styl':
     header(TYPE_CSS);
     files::highlander();
@@ -32,30 +59,8 @@ switch(files::ext($full)) {
         files::delivers($full);
     }
     break;
-  case 'png':
-    header(TYPE_PNG);
-    files::delivers($full);
-    break; 
-  case 'cur':
-    header(TYPE_ICO);
-    files::delivers($full);
-    break; 
-  case 'ico':
-    header(TYPE_ICO);
-    files::delivers($full);
-    break; 
-  case 'gif':
-    header("Content-type:image/gif");
-    files::delivers($full);
-    break; 
-  case 'jpeg': case 'jpg' :
-    header(TYPE_JPEG);
-    files::delivers($full);
-    break; 
-  case 'ttf' :
-    header("Content-type:application/x-font-ttf");
-    files::delivers($full);
-    break; 
+  default:
+    throw new Exception("Cannot render file of type $ext");
 }
 
 
